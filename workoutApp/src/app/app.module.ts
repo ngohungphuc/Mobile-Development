0252e53580bcb0483203e68a672ab618099d7149
Ngo Hung Phuc <ngohungphuc95@gmail.com>
2017-03-05 20:18:58 +0700

    Add new tabs

--- workoutApp/src/app/app.module.ts
+++ workoutApp/src/app/app.module.ts
@@ -5,26 +5,28 @@ import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
-
+import {WorkoutsPage} from '../pages/workouts/workouts';
 @NgModule({
-  declarations: [
-    MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
-    TabsPage
-  ],
-  imports: [
-    IonicModule.forRoot(MyApp)
-  ],
-  bootstrap: [IonicApp],
-  entryComponents: [
-    MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
-    TabsPage
-  ],
-  providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
+	declarations: [
+		MyApp,
+		AboutPage,
+		ContactPage,
+		HomePage,
+		TabsPage,
+		WorkoutsPage
+	],
+	imports: [
+		IonicModule.forRoot(MyApp)
+	],
+	bootstrap: [IonicApp],
+	entryComponents: [
+		MyApp,
+		AboutPage,
+		ContactPage,
+		HomePage,
+		TabsPage,
+		WorkoutsPage
+	],
+	providers: [{ provide: ErrorHandler, useClass: IonicErrorHandler }]
 })
-export class AppModule {}
+export class AppModule { }
--- workoutApp/src/pages/tabs/tabs.ts
+++ workoutApp/src/pages/tabs/tabs.ts
@@ -3,18 +3,19 @@ import { Component } from '@angular/core';
 import { HomePage } from '../home/home';
 import { AboutPage } from '../about/about';
 import { ContactPage } from '../contact/contact';
-
+import {WorkoutsPage} from '../workouts/workouts';
 @Component({
-  templateUrl: 'tabs.html'
+	templateUrl: 'tabs.html'
 })
 export class TabsPage {
-  // this tells the tabs component which Pages
-  // should be each tab's root Page
-  tab1Root: any = HomePage;
-  tab2Root: any = AboutPage;
-  tab3Root: any = ContactPage;
+	// this tells the tabs component which Pages
+	// should be each tab's root Page
+	tab1Root: any = HomePage;
+	tab2Root: any = AboutPage;
+	tab3Root: any = ContactPage;
+	workoutRoot: any = WorkoutsPage;
 
-  constructor() {
+	constructor() {
 
-  }
+	}
 }
