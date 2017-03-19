19231680b556df2895db0daa136e137ee7ecf71f
Ngo Hung Phuc <ngohungphuc95@gmail.com>
2017-03-19 23:42:42 +0700

    Weather services

---
+++ weatherApp/src/app/services/weather.service.ts
@@ -0,0 +1,9 @@
+import {Injectable,Inject} from '@angular/core';
+import {Http} from '@angular/http';
+import {Observable} from 'rxjs/Observable';
+import 'rxjs/Rx';
+
+@Injectable()
+export class WeatherService {
+    
+}
