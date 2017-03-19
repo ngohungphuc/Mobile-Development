f662c866d3a069694e24dd52a0482d49523a22d9
Ngo Hung Phuc <ngohungphuc95@gmail.com>
2017-03-19 22:47:40 +0700

    Setup view and api key

---
+++ weatherApp/src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { NgModule, ErrorHandler } from '@angular/core';
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { MyApp } from './app.component';
+import { AboutPage } from '../pages/about/about';
+import { ContactPage } from '../pages/contact/contact';
+import { HomePage } from '../pages/home/home';
+import { TabsPage } from '../pages/tabs/tabs';
+import { WeatherPage } from '../pages/weather/weather';
+import { SettingPage } from '../pages/setting/setting';
+@NgModule({
+  declarations: [
+    MyApp,
+    AboutPage,
+    ContactPage,
+    HomePage,
+    WeatherPage,
+    SettingPage,
+    TabsPage,
+  ],
+  imports: [
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    AboutPage,
+    ContactPage,
+    HomePage,
+    WeatherPage,
+    SettingPage,
+    TabsPage,
+  ],
+  providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
+})
+export class AppModule {}
