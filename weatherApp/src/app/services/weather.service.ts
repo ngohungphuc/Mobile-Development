1a05412c2d7bee4d3a5b90580ad79300f2938c4f
hoangphucvu <ngohungphuc95@gmail.com>
2017-03-20 12:02:43 +0700

    Get weather data

--- weatherApp/src/app/services/weather.service.ts
+++ weatherApp/src/app/services/weather.service.ts
@@ -5,5 +5,16 @@ import 'rxjs/Rx';
 
 @Injectable()
 export class WeatherService {
-    
+    apiKey:string;
+    conditionUrl:string;
+    constructor(public http:Http){
+        this.apiKey = 'a909b9c80b657c73';
+        this.conditionUrl = 'http://api.wunderground.com/api/'+ this.apiKey +'/conditions/q/';
+        console.log('Service connect');
+    }
+
+    getWeather(city,state){
+        return this.http.get(this.conditionUrl + '/'+ state +'/' + city+'.json')
+        .map(res=>res.json());
+    }
 }
--- weatherApp/src/pages/weather/weather.ts
+++ weatherApp/src/pages/weather/weather.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
-
+import { WeatherService} from '../../app/services/weather.service';
+import {Observable} from 'rxjs/Observable';
 /*
   Generated class for the Weather page.
 
@@ -9,14 +10,26 @@ import { NavController, NavParams } from 'ionic-angular';
 */
 @Component({
   selector: 'page-weather',
-  templateUrl: 'weather.html'
+  templateUrl: 'weather.html',
+  providers:[WeatherService]
 })
-export class WeatherPage {
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+export class WeatherPage implements OnInit{
+  city:string;
+  state:string;
+  constructor(public navCtrl: NavController, public navParams: NavParams,public weatherService:WeatherService) {
+    this.city = 'Boston';
+    this.state = 'MA';
+  }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad WeatherPage');
   }
 
+
+  ngOnInit(){
+    this.weatherService.getWeather(this.city,this.state)
+    .subscribe(weather=>{
+      console.log(weather);
+    });
+  }
 }
