c6c2b15c212ff5659e33eb247060765b68c8d309
ngohungphuc <ngohungphuc95@gmail.com>
2017-04-07 14:38:11 +0700

    loading component

--- elite-schedule-app/src/pages/tournaments/tournaments.ts
+++ elite-schedule-app/src/pages/tournaments/tournaments.ts
@@ -1,5 +1,5 @@
 ﻿import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, LoadingController } from 'ionic-angular';
 import { MyTeamsPage, TeamsPage } from '../pages';
 import { EliteApi } from '../../shared/shared';
 /**
@@ -15,14 +15,23 @@ import { EliteApi } from '../../shared/shared';
 })
 export class TournamentsPage {
     tournaments: any
-    constructor(public navCtrl: NavController, public navParams: NavParams, private eliteApi: EliteApi) {
+    constructor(public navCtrl: NavController, public navParams: NavParams, private eliteApi: EliteApi, private loadingController: LoadingController) {
     }
 
     //ionViewDidLoad() {
     //    this.eliteApi.getTournament().then(data => this.tournaments = data)
     //}
     ionViewWillEnter() {
-        this.eliteApi.getTournaments().then(data => this.tournaments = data)
+        let loader = this.loadingController.create({
+            content: 'Getting tournaments.....',
+            //spinner:'dots'
+        })
+        loader.present().then(() => {
+            this.eliteApi.getTournaments().then(data => {
+                this.tournaments = data
+                loader.dismiss()
+            })
+        })
     }
     itemTapped($event, tournament) {
         this.navCtrl.push(TeamsPage, tournament)
