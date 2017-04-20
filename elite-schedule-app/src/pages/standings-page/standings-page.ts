e5d858e5ce5b169393f249c6d22925643fb49ae2
ngohungphuc <ngohungphuc95@gmail.com>
2017-04-20 14:49:52 +0700

    update code

--- elite-schedule-app/src/pages/standings-page/standings-page.ts
+++ elite-schedule-app/src/pages/standings-page/standings-page.ts
@@ -1,23 +1,20 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import { EliteApi } from '../../shared/shared';
-import * as _ from 'lodash';
-/**
- * Generated class for the StandingsPage page.
- *
- * See http://ionicframework.com/docs/components/#navigation for more info
- * on Ionic pages and navigation.
- */
+import {Component }from '@angular/core'; 
+import {IonicPage, NavController, NavParams }from 'ionic-angular'; 
+import {EliteApi }from '../../shared/shared'; 
+import * as _ from 'lodash'; 
+
 @IonicPage()
-@Component({
-  selector: 'page-standings-page',
-  templateUrl: 'standings-page.html',
+@Component( {
+  selector:'page-standings-page', 
+  templateUrl:'standings-page.html', 
 })
 export class StandingsPage {
-  allStandings: any[]
-  standings: any[]
-  team: any
-  constructor(public navCtrl: NavController, public navParams: NavParams, private eliteApi: EliteApi) {
+  allStandings:any[]
+  standings:any[]
+  team:any
+  divisionFilter = 'division'
+
+  constructor(public navCtrl:NavController, public navParams:NavParams, private eliteApi:EliteApi) {
   }
 
   ionViewDidLoad() {
@@ -25,14 +22,29 @@ export class StandingsPage {
     let tournamentData = this.eliteApi.getCurrentTournament()
     this.standings = tournamentData.standings
 
-    this.allStandings = _.chain(this.standings)
+    /*this.allStandings = _.chain(this.standings)
       .groupBy('division')
       .toPairs()
       .map(item => _.zipObject(['divisionName', 'divisionStandings'], item))
       .value()
-
-    console.log('this standings', this.standings)
-    console.log('division standing', this.allStandings)
+    */
+    
+    this.allStandings = tournamentData.standings
+    this.filterDivision()
   }
 
+  filterDivision() {
+    if (this.divisionFilter === 'all') {
+      this.standings = this.allStandings; 
+    }else {
+      this.standings = _.filter(this.allStandings, s => s.division === this.team.division); 
+    }
+  }
+
+  getHeader(record, recordIndex, records) {
+    if (recordIndex % 10 === 0 || record.division !== records[recordIndex-1].division) {
+      return record.division
+    }
+    return null
+  }
 }
