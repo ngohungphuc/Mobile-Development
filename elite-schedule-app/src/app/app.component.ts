21dab59652493bc54fce84bb3f85f9d152e31796
ngohungphuc <ngohungphuc95@gmail.com>
2017-04-20 11:47:42 +0700

    events

--- elite-schedule-app/src/app/app.component.ts
+++ elite-schedule-app/src/app/app.component.ts
@@ -1,39 +1,49 @@
-﻿import { Component, ViewChild } from '@angular/core';
-import { Nav, Platform } from 'ionic-angular';
-import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
-import { TournamentsPage, MyTeamsPage } from '../pages/pages';
-import { EliteApi,UserSettings } from '../shared/shared';
-@Component({
-    templateUrl: 'app.html',
-    providers: [
-        EliteApi,
+﻿import {Component, ViewChild }from '@angular/core'; 
+import {Events, Nav, Platform, LoadingController }from 'ionic-angular'; 
+import {StatusBar }from '@ionic-native/status-bar'; 
+import {SplashScreen }from '@ionic-native/splash-screen'; 
+import {TournamentsPage, TeamHomePage, MyTeamsPage }from '../pages/pages'; 
+import {EliteApi, UserSettings }from '../shared/shared'; 
+@Component( {
+    templateUrl:'app.html', 
+    providers:[
+        EliteApi, 
         UserSettings
     ]
 })
 export class MyApp {
-    @ViewChild(Nav) nav: Nav;
+    @ViewChild(Nav)nav:Nav
 
-    rootPage: any = MyTeamsPage;
+    rootPage:any = MyTeamsPage
+    favoriteTeams:any[]
+    pages:Array <  {title:string, component:any } > 
 
-    pages: Array<{ title: string, component: any }>;
-
-    constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen) {
-        this.initializeApp();
+    constructor(
+        public platform:Platform, 
+        public statusBar:StatusBar, 
+        public splashScreen:SplashScreen, 
+        private userSettings:UserSettings, 
+        private loadingController:LoadingController, 
+        private eliteApi:EliteApi, 
+        private events:Events) {
+        this.initializeApp(); 
 
         // used for an example of ngFor and navigation
         this.pages = [
             //{ title: 'List', component: ListPage }
-        ];
+        ]; 
     }
 
     initializeApp() {
-        this.platform.ready().then(() => {
+        this.platform.ready().then(() =>  {
             // Okay, so the platform is ready and our plugins are available.
             // Here you can do any higher level native things you might need.
-            this.statusBar.styleDefault();
-            this.splashScreen.hide();
-        });
+            this.statusBar.styleDefault()
+            this.splashScreen.hide()
+            this.refreshFavorites()
+            //invoke refreshFavorites whenever favorite change
+            this.events.subscribe('favorites:changed',()=> this.refreshFavorites())
+        }); 
     }
 
     goHome() {
@@ -42,5 +52,18 @@ export class MyApp {
 
     goToTournaments() {
         this.nav.push(TournamentsPage)
+    }
+
+    refreshFavorites() {
+        this.favoriteTeams = this.userSettings.getAllFavorite()
+    }
+
+    goToTeam(fav) {
+        let loader = this.loadingController.create( {
+            content:'Getting Data....', 
+            dismissOnPageChange:true
+        })
+        loader.present()
+        this.eliteApi.getTournamentData(fav.tournamentId).subscribe(l => this.nav.push(TeamHomePage, fav.team))
     }
 }
--- elite-schedule-app/src/shared/user.settings.service.ts
+++ elite-schedule-app/src/shared/user.settings.service.ts
@@ -1,20 +1,22 @@
+import {Events }from 'ionic-angular'; 
 import {Injectable }from '@angular/core'; 
 import {Storage }from '@ionic/storage'; 
-import * as _ from "lodash"; 
 @Injectable()
 export class UserSettings {
     
-    constructor(private storage:Storage) {}
+    constructor(private storage:Storage, private events:Events) {}
 
     ngOnInit() {}
 
     favoriteTeam(team, tournamentId, tournamentName) {
         let item =  {team:team, tournamentId:tournamentId, tournamentName:tournamentName}
         this.storage.set(team.id, JSON.stringify(item))
+        this.events.publish('favorites:changed')
     }
 
     unfavoriteTeam(team) {
         this.storage.remove(team.id)
+        this.events.publish('favorites:changed')
     }
 
     isFavoriteTeam(teamId) {
@@ -23,7 +25,7 @@ export class UserSettings {
 
     getAllFavorite() {
         let items = []
-        this.storage.forEach( (value, key, index) => {
+        this.storage.forEach((value, key, index) =>  {
              items.push(JSON.parse(value))
         })
         return items
