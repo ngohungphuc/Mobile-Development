a1b0c74df86037dffc137c04ca53631c5fa8eb20
ngohungphuc <ngohungphuc95@gmail.com>
2017-09-23 21:08:36 +0700

    View model

---
+++ Xamarin/Demo/Demo/Demo/ViewModel/MainPageViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.ViewModel
+{
+    public class MainPageViewModel
+    {
+        public string Prompt { get; set; }
+        public string Name { get; set; }
+
+        public MainPageViewModel()
+        {
+            Prompt = "Full name";
+            Name = "Tony Hudson";
+        }
+    }
+}
--- Xamarin/Demo/Demo/Demo/Views/MainPage.xaml.cs
+++ Xamarin/Demo/Demo/Demo/Views/MainPage.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Demo.ViewModel;
 using Demo.Views;
 using Xamarin.Forms;
 
@@ -10,9 +11,13 @@ namespace Demo
 {
     public partial class MainPage : ContentPage
     {
+        private MainPageViewModel vm;
         public MainPage()
         {
             InitializeComponent();
+            vm = new MainPageViewModel();
+            //tell the page where to bind to
+            BindingContext = vm;
         }
 
         private void Button_OnClicked(object sender, EventArgs e)
