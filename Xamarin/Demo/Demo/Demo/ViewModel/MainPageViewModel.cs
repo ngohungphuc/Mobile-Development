c27b66a4ad19cb6acde74697756d6d6b4ca861f5
ngohungphuc <ngohungphuc95@gmail.com>
2017-09-23 22:17:49 +0700

    binding to other view

--- Xamarin/Demo/Demo/Demo/ViewModel/MainPageViewModel.cs
+++ Xamarin/Demo/Demo/Demo/ViewModel/MainPageViewModel.cs
@@ -1,13 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using Demo.Annotations;
+using Xamarin.Forms;
 
 namespace Demo.ViewModel
 {
-    public class MainPageViewModel
+    public class MainPageViewModel : INotifyPropertyChanged
     {
+        private string labelText = "Hello";
+
+        public string LabelText
+        {
+            get { return labelText; }
+            set
+            {
+                labelText = value;
+                RaisePropertyChanged();
+            }
+        }
         public string Prompt { get; set; }
         public string Name { get; set; }
 
@@ -15,6 +30,26 @@ namespace Demo.ViewModel
         {
             Prompt = "Full name";
             Name = "Tony Hudson";
+        }
+
+        private Command changeTextCommand;
+
+        public Command ChangeTextCommand
+        {
+            get
+            {
+                return changeTextCommand ?? (changeTextCommand = new Command(() =>
+                {
+                    LabelText = " Goodbye";
+                }));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void RaisePropertyChanged([CallerMemberName] string caller = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(caller));
         }
     }
 }
