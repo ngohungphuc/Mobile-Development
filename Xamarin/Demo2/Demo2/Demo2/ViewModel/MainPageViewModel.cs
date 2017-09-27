19aeb3284b2ddeab8d2999640b07451f10368c83
ngohungphuc <ngohungphuc95@gmail.com>
2017-09-28 00:23:13 +0700

    binding collection

---
+++ Xamarin/Demo2/Demo2/Demo2/ViewModel/MainPageViewModel.cs
@@ -0,0 +1,28 @@
+﻿
+using ListView.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListView.ViewModel
+{
+    public class MainPageViewModel
+    {
+        public ObservableCollection<Person> People { get; set; } = new ObservableCollection<Person>();
+
+        public MainPageViewModel()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Person person = new Person();
+                person.Name = "Tony" + i.ToString();
+                person.Address = i.ToString() + "main street";
+                People.Add(person);
+            }
+
+        }
+    }
+}
---
+++ Xamarin/Demo2/Demo2/Demo2/Views/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using ListView.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Demo2
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = new MainPageViewModel();
+        }
+    }
+}
