0dae464934a5e611332b676aef358c35db00c06e
ngohungphuc <ngohungphuc95@gmail.com>
2017-10-05 21:58:09 +0700

    message

---
+++ Xamarin/Message/Message/Message/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Message
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = new MainPageViewModel();
+            
+            MessagingCenter.Subscribe<MainPageViewModel>( new MainPageViewModel(), "ButtonClicked", (sender) =>
+            {
+                DisplayAlert("Message", "Button Clicked", "Ok");
+            });
+        }
+    }
+}
---
+++ Xamarin/Message/Message/Message/MainPageViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace Message
+{
+    public partial class MainPage
+    {
+        public class MainPageViewModel
+        {
+            public ICommand ButtonClickCommand { get; set; }
+
+            public MainPageViewModel()
+            {
+                 ButtonClickCommand = new Command(HandleButtonClick);
+            }
+
+            private void HandleButtonClick()
+            {
+                //publish  message in view model
+                MessagingCenter.Send<MainPageViewModel>(this, "ButtonClicked");
+            }
+        }
+    }
+}
