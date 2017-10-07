c730fbf1710319a071a082ccd7591ea58fc56199
ngohungphuc <ngohungphuc95@gmail.com>
2017-10-07 11:38:35 +0700

    add event method

--- Xamarin/TodoApp/TodoApp/TodoApp/CreatePage.xaml.cs
+++ Xamarin/TodoApp/TodoApp/TodoApp/CreatePage.xaml.cs
@@ -12,9 +12,55 @@ namespace TodoApp
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class CreatePage : ContentPage
     {
+        public List<TodoItem> todoItems;
         public CreatePage()
         {
+            todoItems = new List<TodoItem>();
             InitializeComponent();
         }
+
+        private void OnSave(object sender, EventArgs e)
+        {
+            todoItems.Add(
+                new TodoItem(
+                    Todo.Text,
+                    Priority.Text,
+                    SetDueDate(
+                        Date.Date,
+                        Time.Time.Hours,
+                        Time.Time.Minutes,
+                        Time.Time.Seconds
+                        ),
+                    false
+                    ));
+            Clear();
+        }
+
+        private void OnCancel(object sender, EventArgs e)
+        {
+
+        }
+
+        private void OnReview(object sender, EventArgs e)
+        {
+            var tempList = todoItems;
+            Clear();
+        }
+
+        private void Clear()
+        {
+            Todo.Text = Priority.Text = string.Empty;
+            Time.Time = new TimeSpan(
+                DateTime.Now.Hour,
+                DateTime.Now.Minute,
+                DateTime.Now.Second);
+        }
+
+        private DateTime SetDueDate(DateTime dateDate, int timeHours, int timeMinutes, int timeSeconds)
+        {
+            DateTime val = new DateTime(dateDate.Year, dateDate.Month, dateDate.Day, timeHours, timeMinutes, timeSeconds);
+            return val;
+        }
+
     }
 }
---
+++ Xamarin/TodoApp/TodoApp/TodoApp/TodoItem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoApp
+{
+    public class TodoItem
+    {
+        public string TaskName { get; set; }
+        public string Priority { get; set; }
+        public DateTime DueDate { get; set; }
+        public bool IsDeleted { get; set; }
+
+        public TodoItem(string taskName, string priority, DateTime dueDate, bool isDeleted)
+        {
+            TaskName = taskName;
+            Priority = priority;
+            DueDate = dueDate;
+            IsDeleted = isDeleted;
+        }
+    }
+}
