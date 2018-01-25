c9bea75d8f18edc3b7b7c88722bc43cb7406cc79
Tony Hudson <ngohungphuc95@gmail.com>
2018-01-26 00:15:07 +0700

    update repo

--- ReactNative/albums/App.js
+++ ReactNative/albums/App.js
@@ -8,9 +8,8 @@ import React, { Component } from "react";
 import { Platform, StyleSheet, Text, View } from "react-native";
 import Header from "./src/components/header";
 
-
 export default class App extends Component<{}> {
   render() {
-    return <Header />
+    return <Header headerText={"Albums"} />;
   }
 }
--- ReactNative/albums/src/components/header.js
+++ ReactNative/albums/src/components/header.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { Text, View } from "react-native";
 
-const Header = () => {
+const Header = props => {
   const { textStyle, viewStyle } = styles;
   return (
     <View style={viewStyle}>
-      <Text style={textStyle}>Albums</Text>
+      <Text style={textStyle}>{props.headerText}</Text>
     </View>
   );
 };
