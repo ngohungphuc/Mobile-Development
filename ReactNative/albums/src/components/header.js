81e022c9b0347931c6584a8dbad3dbf862a9777d
Tony Hudson <ngohungphuc95@gmail.com>
2018-01-25 23:57:27 +0700

    style component

--- ReactNative/albums/src/components/header.js
+++ ReactNative/albums/src/components/header.js
@@ -1,8 +1,22 @@
 import React from "react";
-import { Text } from "react-native";
+import { Text, View } from "react-native";
 
 const Header = () => {
-  return <Text>Albums</Text>;
+  const { textStyle, viewStyle } = styles;
+  return (
+    <View style={viewStyle}>
+      <Text style={textStyle}>Albums</Text>
+    </View>
+  );
+};
+
+const styles = {
+  viewStyle: {
+    backgroundColor: '#f8f8f8'
+  },
+  textStyle: {
+    fontSize: 20
+  }
 };
 
 export default Header;
