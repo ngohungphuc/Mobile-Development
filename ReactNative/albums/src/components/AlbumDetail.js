f3652cd4fe1c554d8c29e25eef2bf39c666852df
Tony Hudson <ngohungphuc95@gmail.com>
2018-01-27 22:14:05 +0700

    adding image

--- ReactNative/albums/src/components/AlbumDetail.js
+++ ReactNative/albums/src/components/AlbumDetail.js
@@ -1,18 +1,23 @@
 //import liraries
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, View, Image } from "react-native";
 import Card from "./Card";
 import CardSection from "./CardSection";
 
 // create a component
-const AlbumDetail = props => {
+const AlbumDetail = ({ album }) => {
+  //destruction import
+  const { title, artist, thumbnail_image } = album;
+  const { headerStyle, thumbnailStyle } = styles;
   return (
     <Card>
       <CardSection>
-        <View />
-        <View style={styles.headerStyle}>
-          <Text>{props.album.title}</Text>
-          <Text>{props.album.artist}</Text>
+        <View>
+          <Image style={thumbnailStyle} source={{ uri: thumbnail_image }} />
+        </View>
+        <View style={headerStyle}>
+          <Text>{title}</Text>
+          <Text>{artist}</Text>
         </View>
       </CardSection>
     </Card>
@@ -23,6 +28,11 @@ const styles = {
   headerStyle: {
     flexDirection: "column",
     justifyContent: "space-around"
+  },
+  thumbnailStyle: {
+    height: 50,
+    width: 50
   }
 };
+
 export default AlbumDetail;
