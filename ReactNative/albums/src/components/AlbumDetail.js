6c6de53828fab25464f2343d6e4ba78eaf9b90be
Tony Hudson <ngohungphuc95@gmail.com>
2018-01-28 07:35:56 +0700

    display img with flex attribute

--- ReactNative/albums/src/components/AlbumDetail.js
+++ ReactNative/albums/src/components/AlbumDetail.js
@@ -7,18 +7,27 @@ import CardSection from "./CardSection";
 // create a component
 const AlbumDetail = ({ album }) => {
   //destruction import
-  const { title, artist, thumbnail_image } = album;
-  const { headerStyle, thumbnailStyle } = styles;
+  const { title, artist, thumbnail_image, image } = album;
+  const {
+    headerStyle,
+    thumbnailStyle,
+    thumbnailContainerStyle,
+    headerTextStyle,
+    imageStyle
+  } = styles;
   return (
     <Card>
       <CardSection>
-        <View>
+        <View style={thumbnailContainerStyle}>
           <Image style={thumbnailStyle} source={{ uri: thumbnail_image }} />
         </View>
         <View style={headerStyle}>
-          <Text>{title}</Text>
+          <Text style={headerTextStyle}>{title}</Text>
           <Text>{artist}</Text>
         </View>
+      </CardSection>
+      <CardSection>
+        <Image style={imageStyle} source={{ uri: image }} />
       </CardSection>
     </Card>
   );
@@ -29,9 +38,24 @@ const styles = {
     flexDirection: "column",
     justifyContent: "space-around"
   },
+  headerTextStyle: {
+    fontSize: 18
+  },
   thumbnailStyle: {
     height: 50,
     width: 50
+  },
+  thumbnailContainerStyle: {
+    justifyContent: "center",
+    alignItems: "center",
+    marginLeft: 10,
+    marginRight: 10
+  },
+  imageStyle: {
+    height: 300,
+    //full width image
+    flex: 1,
+    width: null
   }
 };
 
