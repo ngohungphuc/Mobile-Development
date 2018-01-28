92564d2c0d13b6eb537269585d47a67897b4e4ac
Tony Hudson <ngohungphuc95@gmail.com>
2018-01-28 08:27:18 +0700

    add firebase auth app

---
+++ ReactNative/project/auth/App.js
@@ -0,0 +1,18 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from "react";
+import { Text, View } from "react-native";
+
+export default class App extends Component {
+  render() {
+    return (
+      <View>
+        <Text />
+      </View>
+    );
+  }
+}
