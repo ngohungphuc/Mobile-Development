47288ecbc0104017c4d3d0691817b57abde1f851
Tony Hudson <ngohungphuc95@gmail.com>
2018-02-15 00:32:36 +0700

    update selection reducer

--- ReactNative/project/tech_stack/src/reducers/SelectionReducer.js
+++ ReactNative/project/tech_stack/src/reducers/SelectionReducer.js
@@ -1,3 +1,8 @@
-export default () => {
-  return null;
+export default (state = null, action) => {
+  switch (action.type) {
+    case "select_library":
+      return action.payload;
+    default:
+      return state;
+  }
 };
