de6ca34e8a8cae4ac7faf679c99e65dd8885ee98
Tony Hudson <ngohungphuc95@gmail.com>
2018-02-16 15:47:32 +0700

    wiring up action creators

--- ReactNative/project/manager/src/components/LoginForm.js
+++ ReactNative/project/manager/src/components/LoginForm.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 import { View, Text } from "react-native";
 import { Card, CardSection, Input, Button } from "./common";
+import { connect } from "react-redux";
+import { emailChanged } from "../actions";
 
 class LoginForm extends Component {
   onEmailChange(text) {
-
+    this.props.emailChanged(text);
   }
-  
+
   render() {
     return (
       <Card>
@@ -30,4 +32,4 @@ class LoginForm extends Component {
   }
 }
 
-export default LoginForm;
+export default connect(null, { emailChanged })(LoginForm);
---
+++ ReactNative/project/manager/src/reducers/AuthReducer.js
@@ -0,0 +1,9 @@
+const INITIAL_STATE = { email:'' };
+
+export default (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+
+    default:
+      return state;
+  }
+};
