16762574a0a578360eb091eb3a9247fe07e6c7ea
Tony Hudson <ngohungphuc95@gmail.com>
2018-03-06 21:33:38 +0700

    remove err message

--- ReactNative/project/manager/src/reducers/AuthReducer.js
+++ ReactNative/project/manager/src/reducers/AuthReducer.js
@@ -14,7 +14,7 @@ export default (state = INITIAL_STATE, action) => {
     case PASSWORD_CHANGED:
       return { ...state, password: action.payload };
     case LOGIN_USER_SUCCESS:
-      return { ...state, user: action.payload };
+      return { ...state, user: action.payload, error: "" };
     case LOGIN_USER_FAIL:
       return { ...state, error: "Auth failed", password: "" };
     default:
