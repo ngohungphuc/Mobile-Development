b1e16e670f9484dd80ff2b2118525f2e82d82c40
Tony Hudson <ngohungphuc95@gmail.com>
2018-07-21 23:18:41 +0700

    press event

--- ReactNative/project/my-first-app/App.js
+++ ReactNative/project/my-first-app/App.js
@@ -17,11 +17,21 @@ export default class App extends Component {
     });
   };
 
+  placeDeletedHandler = index => {
+    this.setState(prevState => {
+      return {
+        places: prevState.places.filter((place, i) => {
+          return i !== index;
+        })
+      }
+    });
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <PlaceInput onPlaceAdded={this.placeAddedHandler} />
-        <PlaceList places={this.state.places} />
+        <PlaceList places={this.state.places} onItemDeleted={this.placeDeletedHandler} />
       </View>
     );
   }
--- ReactNative/project/my-first-app/src/components/PlaceList/PlaceList.js
+++ ReactNative/project/my-first-app/src/components/PlaceList/PlaceList.js
@@ -5,7 +5,7 @@ import ListItem from '../ListItem/ListItem';
 
 const placeList = props => {
     const placesOutput = props.places.map((place, i) => (
-        <ListItem key={i} placeName={place} onItemPressed={() => alert(i)} />
+        <ListItem key={i} placeName={place} onItemPressed={() => props.onItemDeleted(i)} />
       ));
     return (
         <View style={styles.listContainer}>{placesOutput}</View>
