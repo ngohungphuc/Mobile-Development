560d46e975947375660c664c7e0a9b48bbd347a3
Tony Hudson <ngohungphuc95@gmail.com>
2018-07-27 23:55:05 +0700

    add redux

---
+++ ReactNative/project/my-first-app/src/store/reducers/places.js
@@ -0,0 +1,12 @@
+const initialState = {
+    place: [],
+    selectedPlace: null
+};
+
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        default: return state;
+    }
+};
+
+export default reducer;
