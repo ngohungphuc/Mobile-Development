edd044afc23b5a6deca960e1047c94b19358c219
Tony Hudson <ngohungphuc95@gmail.com>
2018-07-28 00:11:56 +0700

    setup reducer

--- ReactNative/project/my-first-app/src/store/reducers/places.js
+++ ReactNative/project/my-first-app/src/store/reducers/places.js
@@ -1,3 +1,10 @@
+import {
+    ADD_PLACE,
+    DELETE_PLACE,
+    SELECT_PLACE,
+    DESELECT_PLACE
+} from '../actions/actionTypes';
+
 const initialState = {
     place: [],
     selectedPlace: null
@@ -5,7 +12,37 @@ const initialState = {
 
 const reducer = (state = initialState, action) => {
     switch (action.type) {
-        default: return state;
+        case ADD_PLACE:
+            return {
+                ...state,
+                places: state.places.concat({
+                    key: Math.random(),
+                    placeName: action.placeName,
+                    placeImage
+                })
+            };
+        case DELETE_PLACE:
+            return {
+                ...state,
+                places: state.places.filter(place => {
+                    return place.key !== state.selectedPlace.key;
+                }),
+                selectedPlace: null
+            }
+        case SELECT_PLACE:
+            return {
+                ...state,
+                selectedPlace: state.places.find(place => {
+                    return place.key === key
+                })
+            }
+        case DESELECT_PLACE:
+            return {
+                ...state,
+                selectedPlace: null
+            }
+        default:
+            return state;
     }
 };
 
