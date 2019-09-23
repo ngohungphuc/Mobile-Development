1a94d419d11b40a5a980e749f376bb97682d133d
ngohungphuc <ngohungphuc95@gmail.com>
2019-09-23 17:48:22 +0700

    style layout

---
+++ NativeScript/source-code/platforms/android/app/src/main/assets/app/bundle.db6c526e56b74d7c595d.hot-update.js
@@ -0,0 +1,11 @@
+webpackHotUpdate("bundle",{
+
+/***/ "./app/challenges/current-challenge/current-challenge.component.html":
+/***/ (function(module, exports) {
+
+module.exports = "<TextField\n  row=\"0\"\n  col=\"0\"\n  class=\"form-control\"\n  hint=\"Challenge title\"\n  [(ngModel)]=\"challengeDescription\"\n></TextField>\n<Button\n  class=\"btn\"\n  row=\"0\"\n  col=\"1\"\n  (tap)=\"onSetChallenge()\"\n  text=\"Set Challenge\"\n></Button>\n<Label row=\"1\" col=\"0\" colSpan=\"2\" class=\"title\" [text]=\"currentChallenge\"></Label>\n"
+
+/***/ })
+
+})
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly8vLi9hcHAvY2hhbGxlbmdlcy9jdXJyZW50LWNoYWxsZW5nZS9jdXJyZW50LWNoYWxsZW5nZS5jb21wb25lbnQuaHRtbCJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOzs7OztBQUFBLDZYIiwiZmlsZSI6ImJ1bmRsZS5kYjZjNTI2ZTU2Yjc0ZDdjNTk1ZC5ob3QtdXBkYXRlLmpzIiwic291cmNlc0NvbnRlbnQiOlsibW9kdWxlLmV4cG9ydHMgPSBcIjxUZXh0RmllbGRcXG4gIHJvdz1cXFwiMFxcXCJcXG4gIGNvbD1cXFwiMFxcXCJcXG4gIGNsYXNzPVxcXCJmb3JtLWNvbnRyb2xcXFwiXFxuICBoaW50PVxcXCJDaGFsbGVuZ2UgdGl0bGVcXFwiXFxuICBbKG5nTW9kZWwpXT1cXFwiY2hhbGxlbmdlRGVzY3JpcHRpb25cXFwiXFxuPjwvVGV4dEZpZWxkPlxcbjxCdXR0b25cXG4gIGNsYXNzPVxcXCJidG5cXFwiXFxuICByb3c9XFxcIjBcXFwiXFxuICBjb2w9XFxcIjFcXFwiXFxuICAodGFwKT1cXFwib25TZXRDaGFsbGVuZ2UoKVxcXCJcXG4gIHRleHQ9XFxcIlNldCBDaGFsbGVuZ2VcXFwiXFxuPjwvQnV0dG9uPlxcbjxMYWJlbCByb3c9XFxcIjFcXFwiIGNvbD1cXFwiMFxcXCIgY29sU3Bhbj1cXFwiMlxcXCIgY2xhc3M9XFxcInRpdGxlXFxcIiBbdGV4dF09XFxcImN1cnJlbnRDaGFsbGVuZ2VcXFwiPjwvTGFiZWw+XFxuXCIiXSwic291cmNlUm9vdCI6IiJ9
