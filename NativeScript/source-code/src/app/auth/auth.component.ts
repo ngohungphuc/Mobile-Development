104410041e5286808ccf827be60b9012f10326ab
ngohungphuc <ngohungphuc95@gmail.com>
2019-09-24 21:37:30 +0700

    update

--- NativeScript/source-code/src/app/auth/auth.component.ts
+++ NativeScript/source-code/src/app/auth/auth.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { RouterExtensions } from "nativescript-angular/router";
 
 @Component({
-  selector: 'ns-auth',
-  templateUrl: './auth.component.html',
-  styleUrls: ['./auth.component.css']
+  selector: "ns-auth",
+  templateUrl: "./auth.component.html",
+  styleUrls: ["./auth.component.css"]
 })
 export class AuthComponent implements OnInit {
+  constructor(private router: RouterExtensions) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  goSignin() {
+    // clear history to remove caching of previous page in the stack
+    this.router.navigate(["/today"], { clearHistory: true });
   }
-
 }
