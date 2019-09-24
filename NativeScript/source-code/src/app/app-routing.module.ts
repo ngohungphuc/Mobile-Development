7d0ee361b1aec2313cccbf79d1ba4005f5439b72
ngohungphuc <ngohungphuc95@gmail.com>
2019-09-24 21:52:09 +0700

    add back menu for android

--- NativeScript/source-code/src/app/app-routing.module.ts
+++ NativeScript/source-code/src/app/app-routing.module.ts
@@ -1,13 +1,17 @@
-import { NgModule } from '@angular/core';
-import { NativeScriptRouterModule } from 'nativescript-angular/router';
-import { Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { NativeScriptRouterModule } from "nativescript-angular/router";
+import { Routes } from "@angular/router";
 
-import { AuthComponent } from './auth/auth.component';
-import { TodayComponent } from './challenges/today/today.component';
+import { AuthComponent } from "./auth/auth.component";
+import { TodayComponent } from "./challenges/today/today.component";
+import { CurrentChallengeComponent } from "./challenges/current-challenge/current-challenge.component";
+import { ChallengeEditComponent } from "./challenges/challenge-edit/challenge-edit.component";
 
 const routes: Routes = [
-    { path: '', component: AuthComponent },
-    { path: 'today', component: TodayComponent }
+  { path: "", component: CurrentChallengeComponent },
+  { path: "today", component: TodayComponent },
+  { path: "current-challenge", component: CurrentChallengeComponent },
+  { path: "edit-challenge", component: ChallengeEditComponent }
 ];
 
 @NgModule({
