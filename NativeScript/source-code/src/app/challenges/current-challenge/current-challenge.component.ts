031c3e109eced63ea4fb86522257fe2930738643
ngohungphuc <ngohungphuc95@gmail.com>
2019-09-24 22:16:58 +0700

    add action bar and back button

--- NativeScript/source-code/src/app/challenges/current-challenge/current-challenge.component.ts
+++ NativeScript/source-code/src/app/challenges/current-challenge/current-challenge.component.ts
@@ -1,9 +1,33 @@
 import { Component } from "@angular/core";
+import { Router } from "@angular/router";
+import { isAndroid } from "platform";
+import { Page } from "tns-core-modules/ui/page/page";
+
+declare var android: any;
 
 @Component({
   selector: "ns-current-challenge",
   templateUrl: "./current-challenge.component.html",
-  styleUrls: ["./current-challenge.component.css"],
-  
+  styleUrls: ["./current-challenge.component.css"]
 })
-export class CurrentChallengeComponent {}
+export class CurrentChallengeComponent {
+  constructor(private router: Router, private page: Page) {}
+
+  onEdit() {
+    this.router.navigate(["/edit-challenge"]);
+  }
+
+  onLoaded() {
+    if (isAndroid) {
+      const androidToolbar = this.page.actionBar.nativeView;
+      const backButton = androidToolbar.getNavigationIcon();
+
+      if (backButton) {
+        backButton.setColorFilter(
+          android.graphics.Color.parseColor("#171717"),
+          (<any>android.graphics).PorterDuff.Mode.SRC_ATOP
+        );
+      }
+    }
+  }
+}
