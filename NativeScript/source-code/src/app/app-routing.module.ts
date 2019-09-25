3a8b19c72d343d0f89ad38b589ed45e9b96fe62a
ngohungphuc <ngohungphuc95@gmail.com>
2019-09-25 14:20:30 +0700

    add tabview

--- NativeScript/source-code/src/app/app-routing.module.ts
+++ NativeScript/source-code/src/app/app-routing.module.ts
@@ -6,12 +6,14 @@ import { AuthComponent } from "./auth/auth.component";
 import { TodayComponent } from "./challenges/today/today.component";
 import { CurrentChallengeComponent } from "./challenges/current-challenge/current-challenge.component";
 import { ChallengeEditComponent } from "./challenges/challenge-edit/challenge-edit.component";
+import { ChallengeTabsComponent } from "./challenges/challenge-tabs/challenge-tabs.component";
 
 const routes: Routes = [
-  { path: "", component: CurrentChallengeComponent },
+  { path: "", component: AuthComponent },
   { path: "today", component: TodayComponent },
   { path: "current-challenge", component: CurrentChallengeComponent },
-  { path: "edit-challenge", component: ChallengeEditComponent }
+  { path: "edit-challenge", component: ChallengeEditComponent },
+  { path: "challenges", component: ChallengeTabsComponent }
 ];
 
 @NgModule({
---
+++ NativeScript/source-code/src/app/challenges/challenge-tabs/challenge-tabs.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ns-challenge-tabs',
+  templateUrl: './challenge-tabs.component.html',
+  styleUrls: ['./challenge-tabs.component.css']
+})
+export class ChallengeTabsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
