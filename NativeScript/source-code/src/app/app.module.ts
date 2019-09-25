23dfd09f5e78213c05bcf9d3adf622c09117fb56
ngohungphuc <ngohungphuc95@gmail.com>
2019-09-25 22:31:55 +0700

    add side drawer

--- NativeScript/source-code/src/app/app.module.ts
+++ NativeScript/source-code/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
 import { NativeScriptModule } from "nativescript-angular/nativescript.module";
 import { NativeScriptFormsModule } from "nativescript-angular/forms";
+import { NativeScriptUISideDrawerModule } from "nativescript-ui-sidedrawer/angular/side-drawer-directives";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
@@ -19,7 +20,12 @@ import { ActionBarComponent } from "./shared/ui/action-bar/action-bar.component"
 
 @NgModule({
   bootstrap: [AppComponent],
-  imports: [NativeScriptModule, NativeScriptFormsModule, AppRoutingModule],
+  imports: [
+    NativeScriptModule,
+    NativeScriptFormsModule,
+    AppRoutingModule,
+    NativeScriptUISideDrawerModule
+  ],
   declarations: [
     AppComponent,
     CurrentChallengeComponent,
