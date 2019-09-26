4c52fd44643e1fa5a03349aad3844f9f8b14996d
ngohungphuc <ngohungphuc95@gmail.com>
2019-09-26 11:58:08 +0700

    add

--- NativeScript/source-code/src/app/app.component.ts
+++ NativeScript/source-code/src/app/app.component.ts
@@ -1,20 +1,36 @@
-import { Component, OnInit, OnDestroy } from "@angular/core";
+import {
+  Component,
+  OnInit,
+  OnDestroy,
+  ViewChild,
+  AfterViewInit,
+  ChangeDetectorRef
+} from "@angular/core";
 import { Subscription } from "rxjs";
-
+import { RadSideDrawerComponent } from "nativescript-ui-sidedrawer/angular/side-drawer-directives";
 import { UIService } from "./shared/ui.service";
 
 @Component({
   selector: "ns-app",
   templateUrl: "./app.component.html"
 })
-export class AppComponent implements OnInit, OnDestroy {
+export class AppComponent implements OnInit, OnDestroy, AfterViewInit {
+  @ViewChild(RadSideDrawerComponent, { static: false })
+  drawerComponent: RadSideDrawerComponent;
   private drawerSub: Subscription;
-  constructor(private uIService: UIService) {}
 
-  ngOnInit(): void {
-    this.drawerSub = this.uIService.drawerState.subscribe(x=> {
-      console.log('x');
+  constructor(
+    private uIService: UIService,
+    private changeDectionRef: ChangeDetectorRef
+  ) {}
+
+  ngOnInit(): void {}
+
+  ngAfterViewInit(): void {
+    this.drawerSub = this.uIService.drawerState.subscribe(_ => {
+      this.drawerComponent.sideDrawer.toggleDrawerState();
     });
+    this.changeDectionRef.detectChanges();
   }
 
   ngOnDestroy(): void {
