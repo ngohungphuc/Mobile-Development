09bbf04a2284c7617c9f0305a8da393173548d86
ngohungphuc <ngohungphuc95@gmail.com>
2019-09-26 16:19:08 +0700

    u

--- NativeScript/source-code/src/app/challenges/challenge-edit/challenge-edit.component.ts
+++ NativeScript/source-code/src/app/challenges/challenge-edit/challenge-edit.component.ts
@@ -1,20 +1,24 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { PageRoute } from "nativescript-angular/router";
 
 @Component({
   selector: "ns-challenge-edit",
   templateUrl: "./challenge-edit.component.html",
-  styleUrls: ["./challenge-edit.component.css"],
-  
+  styleUrls: ["./challenge-edit.component.css"]
 })
-export class ChallengeEditComponent implements OnInit{
-  constructor(private activatedRoute: ActivatedRoute) {
-
-  }
+export class ChallengeEditComponent implements OnInit {
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private pageRoute: PageRoute
+  ) {}
 
   ngOnInit(): void {
-    this.activatedRoute.paramMap.subscribe(paramMap => {
-      
+    /* this.activatedRoute.paramMap.subscribe(paramMap => {}); */
+    this.pageRoute.activatedRoute.subscribe(activatedRoute => {
+      activatedRoute.paramMap.subscribe(paramMap => {
+        console.log(paramMap.get("mode"));
+      });
     });
   }
 }
