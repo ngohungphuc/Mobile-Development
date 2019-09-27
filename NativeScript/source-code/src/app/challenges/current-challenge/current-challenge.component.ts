19a4bd133d9dd852f23dbc2f4334e2b67cce2b92
ngohungphuc <ngohungphuc95@gmail.com>
2019-09-27 17:42:58 +0700

    open a modal

--- NativeScript/source-code/src/app/challenges/current-challenge/current-challenge.component.ts
+++ NativeScript/source-code/src/app/challenges/current-challenge/current-challenge.component.ts
@@ -1,17 +1,30 @@
-import { Component } from "@angular/core";
+import { Component, ViewContainerRef } from "@angular/core";
 import { RouterExtensions } from "nativescript-angular/router";
+import { ModalDialogService } from "nativescript-angular/modal-dialog";
 
+import { DayModalComponent } from "../day-modal/day-modal.component";
 @Component({
   selector: "ns-current-challenge",
   templateUrl: "./current-challenge.component.html",
   styleUrls: ["./current-challenge.component.css"]
 })
 export class CurrentChallengeComponent {
-  constructor(private router: RouterExtensions) {}
+  constructor(
+    private router: RouterExtensions,
+    private modelDialog: ModalDialogService,
+    private vcRef: ViewContainerRef
+  ) {}
 
   onEdit() {
     this.router.navigate(["/challenges/edit"], {
       transition: { name: "slideLeft" }
+    });
+  }
+
+  onChangeStatus() {
+    this.modelDialog.showModal(DayModalComponent, {
+      fullscreen: true,
+      viewContainerRef: this.vcRef
     });
   }
 }
---
+++ NativeScript/source-code/src/app/challenges/day-modal/day-modal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ns-day-modal',
+  templateUrl: './day-modal.component.html',
+  styleUrls: ['./day-modal.component.css']
+})
+export class DayModalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
