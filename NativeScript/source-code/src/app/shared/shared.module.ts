6f26962d9b607dfb0553349b894d984a557e64f4
ngohungphuc <ngohungphuc95@gmail.com>
2019-09-30 21:22:42 +0700

    update

--- NativeScript/source-code/src/app/shared/shared.module.ts
+++ NativeScript/source-code/src/app/shared/shared.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from "@angular/core";
-import { ActionBarComponent } from "./ui/action-bar/action-bar.component";
 import { NativeScriptCommonModule } from "nativescript-angular/common";
 import { NativeScriptRouterModule } from "nativescript-angular/router";
+
+import { ActionBarComponent } from "./ui/action-bar/action-bar.component";
 
 @NgModule({
   imports: [NativeScriptCommonModule, NativeScriptRouterModule],
