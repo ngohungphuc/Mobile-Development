b250daf35a01b3ddfb0fcc456469036cee326930
ngohungphuc <ngohungphuc95@gmail.com>
2019-12-20 08:58:20 +0700

    styling the layout

--- Flutter/first_app/lib/question.dart
+++ Flutter/first_app/lib/question.dart
@@ -7,6 +7,13 @@ class Question extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text(questionText);
+    return Container(
+        width: double.infinity,
+        margin: EdgeInsets.all(10),
+        child: Text(
+          questionText,
+          style: TextStyle(fontSize: 28),
+          textAlign: TextAlign.center,
+        ));
   }
 }
