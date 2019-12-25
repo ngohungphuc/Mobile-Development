4a3bc6a1e5c17a436b37df9d4dc5762e64bd7152
ngohungphuc <ngohungphuc95@gmail.com>
2019-12-25 09:28:51 +0700

    getter and if else

--- Flutter/first_app/lib/main.dart
+++ Flutter/first_app/lib/main.dart
@@ -16,22 +16,40 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   final _questions = const [
     {
-      'questionText': 'What \'s your favorite color ?',
-      'answers': ['Black', 'Red', 'Green', 'White']
+      'questionText': 'What\'s your favorite color?',
+      'answers': [
+        {'text': 'Black', 'score': 10},
+        {'text': 'Red', 'score': 5},
+        {'text': 'Green', 'score': 3},
+        {'text': 'White', 'score': 1},
+      ],
     },
     {
-      'questionText': 'What \'s your favorite animal ?',
-      'answers': ['Dog', 'Cat', 'Rabbit', 'Turtle']
+      'questionText': 'What\'s your favorite animal?',
+      'answers': [
+        {'text': 'Rabbit', 'score': 3},
+        {'text': 'Snake', 'score': 11},
+        {'text': 'Elephant', 'score': 5},
+        {'text': 'Lion', 'score': 9},
+      ],
     },
     {
-      'questionText': 'Who \'s your favorite friend ?',
-      'answers': ['Dog', 'Cat', 'Rabbit', 'Turtle']
+      'questionText': 'Who\'s your favorite instructor?',
+      'answers': [
+        {'text': 'Max', 'score': 1},
+        {'text': 'Max', 'score': 1},
+        {'text': 'Max', 'score': 1},
+        {'text': 'Max', 'score': 1},
+      ],
     },
   ];
 
   var _questionIndex = 0;
+  var _totalScore = 0;
 
-  void _answerQuestion() {
+  void _answerQuestion(int score) {
+    _totalScore += score;
+
     setState(() {
       _questionIndex = _questionIndex + 1;
     });
@@ -52,7 +70,7 @@ class _MyAppState extends State<MyApp> {
                 questionIndex: _questionIndex,
                 questions: _questions,
               )
-            : Result(),
+            : Result(_totalScore),
       ),
     );
   }
--- Flutter/first_app/lib/result.dart
+++ Flutter/first_app/lib/result.dart
@@ -1,10 +1,27 @@
 import 'package:flutter/material.dart';
 
 class Result extends StatelessWidget {
+  final int resultScore;
+
+  Result(this.resultScore);
+
+  String get resultPhrase {
+    var resultText = 'You did it';
+    if (resultScore <= 8) {
+      resultText = 'Awesome';
+    } else if (resultScore <= 12) {
+      resultText = 'Pretty';
+    }
+    return resultText;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text('You did it'),
+      child: Text(
+        resultPhrase,
+        style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+      ),
     );
   }
 }
