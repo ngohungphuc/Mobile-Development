5559a666df2ed9bc8377f3604c4eac37092c78bd
ngohungphuc <ngohungphuc95@gmail.com>
2020-01-01 11:14:00 +0700

    UPDATE STLYE

--- Flutter/spend_planner/lib/main.dart
+++ Flutter/spend_planner/lib/main.dart
@@ -45,6 +45,13 @@ class MyHomePage extends StatelessWidget {
                   child: Row(
                 children: <Widget>[
                   Container(
+                    margin: EdgeInsets.symmetric(
+                      vertical: 10,
+                      horizontal: 15,
+                    ),
+                    decoration: BoxDecoration(
+                        border: Border.all(color: Colors.black, width: 2)),
+                    padding: EdgeInsets.all(10),
                     child: Text(tx.amount.toString()),
                   ),
                   Column(
