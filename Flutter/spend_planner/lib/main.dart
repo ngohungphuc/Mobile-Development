1d82e1bdc10a4015e6263e8ce1df4160cf1dda6f
ngohungphuc <ngohungphuc95@gmail.com>
2020-01-07 21:54:07 +0700

    custom font

--- Flutter/spend_planner/lib/main.dart
+++ Flutter/spend_planner/lib/main.dart
@@ -10,7 +10,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Personal Expenses',
-      theme: ThemeData(primarySwatch: Colors.purple, accentColor: Colors.amber),
+      theme: ThemeData(
+          primarySwatch: Colors.purple,
+          accentColor: Colors.amber,
+          fontFamily: 'Quicksand'),
       home: MyHomePage(),
     );
   }
