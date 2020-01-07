ecc4e21a8849f87dd9c6f341064813a34ea04501
ngohungphuc <ngohungphuc95@gmail.com>
2020-01-07 20:46:52 +0700

    add toStringAsFixed

--- Flutter/spend_planner/lib/widgets/transaction_list.dart
+++ Flutter/spend_planner/lib/widgets/transaction_list.dart
@@ -26,7 +26,7 @@ class TransactionList extends StatelessWidget {
                       border: Border.all(color: Colors.purple, width: 2)),
                   padding: EdgeInsets.all(10),
                   child: Text(
-                    '\$${_transaction[index].amount}',
+                    '\$${_transaction[index].amount.toStringAsFixed(2)}',
                     style: TextStyle(
                         fontWeight: FontWeight.bold,
                         fontSize: 20,
