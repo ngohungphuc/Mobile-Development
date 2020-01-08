d89b74da506e3c67f305ca08ca628658ca6176d8
ngohungphuc <ngohungphuc95@gmail.com>
2020-01-08 22:11:56 +0700

    add safe check

--- Flutter/spend_planner/lib/widgets/chart.dart
+++ Flutter/spend_planner/lib/widgets/chart.dart
@@ -42,7 +42,11 @@ class Chart extends StatelessWidget {
       child: Row(
         children: groupedTransactionValues.map((data) {
           return ChartBar(
-              data['day'], data['amount'], (data['amount'] as double) / totalSpending);
+              data['day'],
+              data['amount'],
+              totalSpending == 0.0
+                  ? 0.0
+                  : (data['amount'] as double) / totalSpending);
         }).toList(),
       ),
     );
