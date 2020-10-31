11f791cdcae3a35428c1f4fbabbe30e1d37acd27
ngohungphuc <ngohungphuc95@gmail.com>
2020-10-31 12:06:18 +0700

    update

--- ReactNative/project/2020/rn2020/src/screens/ListScreen.js
+++ ReactNative/project/2020/rn2020/src/screens/ListScreen.js
@@ -11,7 +11,7 @@ const ListScreen = () => {
   ];
   return (
     <FlatList
-        keyExtractor={(friend) => friend.name}
+      keyExtractor={(friend) => friend.name}
       data={friends}
       renderItem={({item}) => {
         return <Text>{item.name}</Text>;
