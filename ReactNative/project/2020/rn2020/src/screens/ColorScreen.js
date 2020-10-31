84cd17ed4c2824f4c193ce7c11d55f8653523fb4
ngohungphuc <ngohungphuc95@gmail.com>
2020-10-31 18:40:25 +0700

    displaya color

--- ReactNative/project/2020/rn2020/src/screens/ColorScreen.js
+++ ReactNative/project/2020/rn2020/src/screens/ColorScreen.js
@@ -1,14 +1,27 @@
 //import liraries
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Button, StyleSheet} from 'react-native';
 
 // create a component
 const ColorScreen = () => {
   return (
     <View>
       <Button title="Add a color"></Button>
+      <View
+        style={{
+          height: 100,
+          width: 100,
+          backgroundColor: randomRgb(),
+        }}></View>
     </View>
   );
+};
+
+const randomRgb = () => {
+  const red = Math.floor(Math.random() * 256);
+  const green = Math.floor(Math.random() * 256);
+  const blue = Math.floor(Math.random() * 256);
+  return `rgb(${red}, ${green}, ${blue})`;
 };
 
 // define your styles
