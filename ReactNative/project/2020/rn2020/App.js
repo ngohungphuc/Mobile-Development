d83f81d02917a5ae0285e0c1725f86101e375ca6
ngohungphuc <ngohungphuc95@gmail.com>
2020-10-31 16:45:42 +0700

    navigation

--- ReactNative/project/2020/rn2020/App.js
+++ ReactNative/project/2020/rn2020/App.js
@@ -12,7 +12,7 @@ const navigator = createStackNavigator(
     List: ListScreen,
   },
   {
-    initialRouteName: 'List',
+    initialRouteName: 'Home',
     defaultNavigationOptions: {
       title: 'App',
     },
--- ReactNative/project/2020/rn2020/src/screens/HomeScreen.js
+++ ReactNative/project/2020/rn2020/src/screens/HomeScreen.js
@@ -1,8 +1,18 @@
 import React from 'react';
-import {Text, StyleSheet} from 'react-native';
+import {Text, StyleSheet, View, Button, TouchableOpacity} from 'react-native';
 
-const HomeScreen = () => {
-  return <Text style={styles.text}>HomeScreen</Text>;
+const HomeScreen = (props) => {
+  return (
+    <View>
+      <Text style={styles.text}>HomeScreen</Text>
+      <Button
+        title="Component page"
+        onPress={() => {console.log('123')}}></Button>
+      <TouchableOpacity onPress={() => props.navigation.navigate('List')}>
+        <Text>Go to list</Text>
+      </TouchableOpacity>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
