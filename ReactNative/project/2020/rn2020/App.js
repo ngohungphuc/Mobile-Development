fb61131ca8dc2edae8baa7b971ebe5791ea4342a
ngohungphuc <ngohungphuc95@gmail.com>
2020-10-31 11:44:52 +0700

    add list

--- ReactNative/project/2020/rn2020/App.js
+++ ReactNative/project/2020/rn2020/App.js
@@ -3,14 +3,15 @@ import {createStackNavigator} from 'react-navigation-stack';
 
 import HomeScreen from './src/screens/HomeScreen';
 import ComponentScreen from './src/screens/ComponentScreen';
-
+import ListScreen from './src/screens/ListScreen';
 const navigator = createStackNavigator(
   {
     Home: HomeScreen,
     Components: ComponentScreen,
+    List: ListScreen,
   },
   {
-    inialRouteName: 'Home',
+    inialRouteName: 'List',
     defaultNavigationOptions: {
       title: 'App',
     },
---
+++ ReactNative/project/2020/rn2020/src/screens/ListScreen.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Text, StyleSheet, View} from 'react-native';
+
+const ListScreen = () => {
+    return <Text>List Screen</Text>
+}
+
+const styles = StyleSheet.create({})
+
+export default ListScreen;
