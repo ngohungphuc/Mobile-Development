3af7949c2a9c2287c5b35714d16a9ed84b5358ab
ngohungphuc <ngohungphuc95@gmail.com>
2020-10-31 17:21:06 +0700

    add image detail

---
+++ ReactNative/project/2020/rn2020/src/components/ImageDetail.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+const ImageDetail = () => {
+  return (
+    <View>
+      <Text>ImageDetail</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default ImageDetail;
--- ReactNative/project/2020/rn2020/src/screens/ImageScreen.js
+++ ReactNative/project/2020/rn2020/src/screens/ImageScreen.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import {Text, StyleSheet, View} from 'react-native';
+import ImageDetail from '../components/ImageDetail';
 
-const ImageScreen = () => (
-  <View>
-    <Text>ImageScreen</Text>
-  </View>
-);
-
+const ImageScreen = () => {
+  return (
+    <View>
+      <ImageDetail></ImageDetail>
+    </View>
+  );
+};
 const styles = StyleSheet.create({});
 
 export default ImageScreen;
