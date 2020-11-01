17a8411be26d6e64205137803114584333797b84
ngohungphuc <ngohungphuc95@gmail.com>
2020-11-01 07:14:38 +0700

    random color

--- ReactNative/project/2020/rn2020/src/screens/ColorScreen.js
+++ ReactNative/project/2020/rn2020/src/screens/ColorScreen.js
@@ -1,12 +1,18 @@
 //import liraries
-import React from 'react';
+import React, {useState} from 'react';
 import {View, Button, StyleSheet} from 'react-native';
 
 // create a component
 const ColorScreen = () => {
+  const [colors, setColors] = useState([]);
+
   return (
     <View>
-      <Button title="Add a color"></Button>
+      <Button
+        title="Add a color"
+        onPress={() => {
+          setColors([...colors, randomRgb()]);
+        }}></Button>
       <View
         style={{
           height: 100,
