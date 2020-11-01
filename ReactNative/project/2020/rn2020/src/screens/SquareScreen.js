9b249f220e894af66882f82b557f0c541f32b11b
ngohungphuc <ngohungphuc95@gmail.com>
2020-11-01 08:41:44 +0700

    update

--- ReactNative/project/2020/rn2020/src/screens/SquareScreen.js
+++ ReactNative/project/2020/rn2020/src/screens/SquareScreen.js
@@ -11,20 +11,41 @@ const SquareScreen = () => {
   const [green, setGreen] = useState(0);
   const [blue, setBlue] = useState(0);
 
+  const setColor = (color, change) => {
+    switch (color) {
+      case 'red':
+        red + change > 255 || red + change < 0 ? null : setRed(red + change);
+      case 'green':
+        green + change > 255 || green + change < 0
+          ? null
+          : setGreen(green + change);
+      case 'blue':
+        blue + change > 255 || blue + change < 0
+          ? null
+          : setBlue(blue + change);
+      default:
+        return;
+    }
+  };
+
   return (
     <View>
       <ColorCounter
         color="Red"
-        onIncrease={() => setRed(red + COLOR_INCREMENT)}
-        onDecrease={() => setRed(red + COLOR_INCREMENT)}></ColorCounter>
+        onIncrease={() => setColor('red', COLOR_INCREMENT)}
+        onDecrease={() => setColor('red', -1 * COLOR_INCREMENT)}></ColorCounter>
       <ColorCounter
         color="Blue"
-        onIncrease={() => setGreen(green + COLOR_INCREMENT)}
-        onDecrease={() => setGreen(green + COLOR_INCREMENT)}></ColorCounter>
+        onIncrease={() => setColor('green', COLOR_INCREMENT)}
+        onDecrease={() =>
+          setColor('green', -1 * COLOR_INCREMENT)
+        }></ColorCounter>
       <ColorCounter
         color="Green"
-        onIncrease={() => setBlue(blue + COLOR_INCREMENT)}
-        onDecrease={() => setBlue(blue + COLOR_INCREMENT)}></ColorCounter>
+        onIncrease={() => setColor('blue', COLOR_INCREMENT)}
+        onDecrease={() =>
+          setColor('blue', -1 * COLOR_INCREMENT)
+        }></ColorCounter>
       <View
         style={{
           height: 150,
