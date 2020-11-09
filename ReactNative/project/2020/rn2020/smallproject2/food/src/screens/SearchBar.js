501731d8b200a01ab54bbc90db823bc4bf0846ec
ngohungphuc <ngohungphuc95@gmail.com>
2020-11-09 22:15:59 +0700

    update

--- ReactNative/project/2020/rn2020/smallproject2/food/src/screens/SearchBar.js
+++ ReactNative/project/2020/rn2020/smallproject2/food/src/screens/SearchBar.js
@@ -1,11 +1,13 @@
 //import liraries
 import React, { Component } from "react";
 import { View, Text, StyleSheet } from "react-native";
+import { Feather } from "@expo/vector-icons";
 
 // create a component
 const SearchBar = () => {
   return (
     <View style={styles.background}>
+      <Feather name="search" size={30}></Feather>
       <Text>SearchBar</Text>
     </View>
   );
