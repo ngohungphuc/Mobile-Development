5fc00feb0211de5b92314d4da55d21017dfe87ed
ngohungphuc <ngohungphuc95@gmail.com>
2021-04-04 22:18:14 +0700

    Add learning project

---
+++ Swift-App/hello-swift/hello-swift/hello_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  hello_swiftApp.swift
+//  hello-swift
+//
+//  Created by Ngo Hung Phuc on 04/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct hello_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
