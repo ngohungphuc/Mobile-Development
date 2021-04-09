b159d45fa8488b495340520ee495d876fef97547
ngohungphuc <ngohungphuc95@gmail.com>
2021-04-09 09:34:41 +0700

    add first app

---
+++ Swift-App/I am Rich/I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Ngo Hung Phuc on 09/04/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
