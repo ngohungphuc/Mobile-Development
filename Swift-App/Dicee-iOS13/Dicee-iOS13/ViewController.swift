99111cc7a0cac05b2a7b99cd1e6b85a0407b0337
ngohungphuc <ngohungphuc95@gmail.com>
2021-04-12 21:46:59 +0700

    add

--- Swift-App/Dicee-iOS13/Dicee-iOS13/ViewController.swift
+++ Swift-App/Dicee-iOS13/Dicee-iOS13/ViewController.swift
@@ -14,8 +14,9 @@ class ViewController: UIViewController {
     @IBOutlet weak var diceImageView2: UIImageView!
     
     @IBAction func rollButtonPressed(_ sender: UIButton) {
-        diceImageView1.image = [#imageLiteral(resourceName: "DiceOne"),#imageLiteral(resourceName: "DiceTwo"),#imageLiteral(resourceName: "DiceThree"),#imageLiteral(resourceName: "DiceFour"),#imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")][ Int.random(in: 1...10)]
-        diceImageView2.image = [#imageLiteral(resourceName: "DiceOne"),#imageLiteral(resourceName: "DiceTwo"),#imageLiteral(resourceName: "DiceThree"),#imageLiteral(resourceName: "DiceFour"),#imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")][ Int.random(in: 1...10)]
+        let diceArray = [#imageLiteral(resourceName: "DiceOne"),#imageLiteral(resourceName: "DiceTwo"),#imageLiteral(resourceName: "DiceThree"),#imageLiteral(resourceName: "DiceFour"),#imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
+        diceImageView1.image = diceArray.randomElement()
+        diceImageView2.image = diceArray.randomElement()
         
         
     }
