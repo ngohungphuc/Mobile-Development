00d40c6bb2b5158ad87a798ebb5c3aeda284fd16
ngohungphuc <ngohungphuc95@gmail.com>
2021-04-23 21:59:28 +0700

    add

--- Swift-App/Xylophone-iOS13/Xylophone/ViewController.swift
+++ Swift-App/Xylophone-iOS13/Xylophone/ViewController.swift
@@ -9,9 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+
+    @IBAction func keyPressed(_ sender: UIButton) {
+        
     }
 
 
