a1a1100f28064970886493713006ca38b4b5d39b
ngohungphuc <ngohungphuc95@gmail.com>
2021-04-24 21:57:06 +0700

    add

--- Swift-App/EggTimer-iOS13/EggTimer/ViewController.swift
+++ Swift-App/EggTimer-iOS13/EggTimer/ViewController.swift
@@ -9,12 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-    let eggTimes = ["Soft": 5, "Medium": 7, "Hard": 12]
+    let eggTimes = ["Soft": 300, "Medium": 420, "Hard": 720]
+    
+    var secondsRemaining = 60
     
     @IBAction func hardnessSelected(_ sender: UIButton) {
         let hardness = sender.currentTitle!
         
-        let result = eggTimes[hardness]
+        secondsRemaining = eggTimes[hardness]!
+        
+        Timer.scheduledTimer(timeInterval: 1.0, target:self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
     }
     
+    
+    @objc func updateTimer() {
+        if secondsRemaining > 0 {
+            secondsRemaining -= 1
+        }
+    }
 }
