95e823942120edc0745f653fd8c6919e6afc26ad
ngohungphuc <ngohungphuc95@gmail.com>
2021-04-24 21:50:57 +0700

    add

--- Swift-App/EggTimer-iOS13/EggTimer/ViewController.swift
+++ Swift-App/EggTimer-iOS13/EggTimer/ViewController.swift
@@ -9,9 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
+    let eggTimes = ["Soft": 5, "Medium": 7, "Hard": 12]
     
-
     @IBAction func hardnessSelected(_ sender: UIButton) {
+        let hardness = sender.currentTitle!
+        
+        let result = eggTimes[hardness]
     }
     
 }
