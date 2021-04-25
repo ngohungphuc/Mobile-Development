1c46713f1add0e58e11de90ae6c3a20a489cfd4c
ngohungphuc <ngohungphuc95@gmail.com>
2021-04-25 21:35:01 +0700

    add

---
+++ Swift-App/BMI-Calculator-iOS13/BMI Calculator/Controllers/SecondViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SecondViewController.swift
+//  BMI Calculator
+//
+//  Created by Ngo Hung Phuc on 25/04/2021.
+//  Copyright © 2021 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
