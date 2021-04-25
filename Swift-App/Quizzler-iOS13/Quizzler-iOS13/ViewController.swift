9a61562b3b8a02aa1ed04f5690c3bc98683253a7
ngohungphuc <ngohungphuc95@gmail.com>
2021-04-25 09:49:30 +0700

    add

--- Swift-App/Quizzler-iOS13/Quizzler-iOS13/ViewController.swift
+++ Swift-App/Quizzler-iOS13/Quizzler-iOS13/ViewController.swift
@@ -16,19 +16,19 @@ class ViewController: UIViewController {
     @IBOutlet weak var falseButton: UIButton!
     
     let quiz = [
-          Question(q: "A slug's blood is green.", a: "True"),
-          Question(q: "Approximately one quarter of human bones are in the feet.", a: "True"),
-          Question(q: "The total surface area of two human lungs is approximately 70 square metres.", a: "True"),
-          Question(q: "In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.", a: "True"),
-          Question(q: "In London, UK, if you happen to die in the House of Parliament, you are technically entitled to a state funeral, because the building is considered too sacred a place.", a: "False"),
-          Question(q: "It is illegal to pee in the Ocean in Portugal.", a: "True"),
-          Question(q: "You can lead a cow down stairs but not up stairs.", a: "False"),
-          Question(q: "Google was originally called 'Backrub'.", a: "True"),
-          Question(q: "Buzz Aldrin's mother's maiden name was 'Moon'.", a: "True"),
-          Question(q: "The loudest sound produced by any animal is 188 decibels. That animal is the African Elephant.", a: "False"),
-          Question(q: "No piece of square dry paper can be folded in half more than 7 times.", a: "False"),
-          Question(q: "Chocolate affects a dog's heart and nervous system; a few ounces are enough to kill a small dog.", a: "True")
-
+        Question(q: "A slug's blood is green.", a: "True"),
+        Question(q: "Approximately one quarter of human bones are in the feet.", a: "True"),
+        Question(q: "The total surface area of two human lungs is approximately 70 square metres.", a: "True"),
+        Question(q: "In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.", a: "True"),
+        Question(q: "In London, UK, if you happen to die in the House of Parliament, you are technically entitled to a state funeral, because the building is considered too sacred a place.", a: "False"),
+        Question(q: "It is illegal to pee in the Ocean in Portugal.", a: "True"),
+        Question(q: "You can lead a cow down stairs but not up stairs.", a: "False"),
+        Question(q: "Google was originally called 'Backrub'.", a: "True"),
+        Question(q: "Buzz Aldrin's mother's maiden name was 'Moon'.", a: "True"),
+        Question(q: "The loudest sound produced by any animal is 188 decibels. That animal is the African Elephant.", a: "False"),
+        Question(q: "No piece of square dry paper can be folded in half more than 7 times.", a: "False"),
+        Question(q: "Chocolate affects a dog's heart and nervous system; a few ounces are enough to kill a small dog.", a: "True")
+        
         
     ]
     
@@ -45,9 +45,9 @@ class ViewController: UIViewController {
         let actualAnswer = quiz[questionNumber].answer
         
         if userAnswer == actualAnswer {
-            
+            sender.backgroundColor = UIColor.green
         } else {
-            
+            sender.backgroundColor = UIColor.red
         }
         
         if questionNumber + 1 < quiz.count {
@@ -57,11 +57,14 @@ class ViewController: UIViewController {
             questionNumber = 0
         }
         
-        updateUI()
+        Timer.scheduledTimer(timeInterval: 0.2,target: self,selector: #selector(updateUI), userInfo: nil, repeats: false)
     }
     
-    func updateUI() {
+    @objc func updateUI() {
         questionLabel.text = quiz[questionNumber].text
+        trueButton.backgroundColor = UIColor.clear
+        falseButton.backgroundColor = UIColor.clear
+        progressBar.progress = Float(questionNumber)/Float(quiz.count)
     }
 }
 
