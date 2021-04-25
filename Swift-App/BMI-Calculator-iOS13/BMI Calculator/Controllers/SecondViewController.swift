6ab69dd17d1a8b87d3cd1f7f98cf91c7a32fcf98
ngohungphuc <ngohungphuc95@gmail.com>
2021-04-25 21:59:31 +0700

    add

--- Swift-App/BMI-Calculator-iOS13/BMI Calculator/Controllers/SecondViewController.swift
+++ Swift-App/BMI-Calculator-iOS13/BMI Calculator/Controllers/SecondViewController.swift
@@ -12,5 +12,10 @@ class SecondViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        view.backgroundColor = .red
+        let label = UILabel()
+        label.text = "Hello"
+        label.frame = CGRect(x: 0, y: 0, width: 100, height: 50)
+        view.addSubview(label)
     }
 }
