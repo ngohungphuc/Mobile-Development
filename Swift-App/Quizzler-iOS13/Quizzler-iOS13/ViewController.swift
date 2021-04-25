72e039a86bb08eabb57678419cd004db5d28d443
ngohungphuc <ngohungphuc95@gmail.com>
2021-04-25 08:11:28 +0700

    add

--- Swift-App/Quizzler-iOS13/Quizzler-iOS13/ViewController.swift
+++ Swift-App/Quizzler-iOS13/Quizzler-iOS13/ViewController.swift
@@ -11,17 +11,31 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var progressBar: UIProgressView!
-    @IBOutlet weak var questionLabel: UILabel!    
+    @IBOutlet weak var questionLabel: UILabel!
     @IBOutlet weak var trueButton: UIButton!
     @IBOutlet weak var falseButton: UIButton!
+    
+    let quiz = [
+        "1",
+        "2",
+        "3"
+        
+    ]
+    
+    var questionNumber = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        updateUI()
     }
     
     @IBAction func answerButtonPressed(_ sender: UIButton) {
+        questionNumber += 1
     }
     
+    func updateUI() {
+        questionLabel.text = quiz[questionNumber]
+    }
 }
 
