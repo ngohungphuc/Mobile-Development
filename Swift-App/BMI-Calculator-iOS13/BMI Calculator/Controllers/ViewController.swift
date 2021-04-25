07796342f9d5492b894c8563f8b2679b1f225143
ngohungphuc <ngohungphuc95@gmail.com>
2021-04-25 20:49:50 +0700

    add

--- Swift-App/BMI-Calculator-iOS13/BMI Calculator/Controllers/ViewController.swift
+++ Swift-App/BMI-Calculator-iOS13/BMI Calculator/Controllers/ViewController.swift
@@ -9,17 +9,21 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
     
-    @IBAction func heightSliderChange(_ sender: Any) {
+    @IBAction func heightSliderChange(_ sender: UISlider) {
+        let height = String(format: "%.2f", sender.value)
+        heightLabel.text = "\(height)"
     }
     
-    @IBAction func weightSliderChange(_ sender: Any) {
+    @IBAction func weightSliderChange(_ sender: UISlider) {
+        weightLabel.text = String(format: "%.0f", sender.value)
     }
-
-
 }
 
