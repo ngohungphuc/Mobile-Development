44c69df0a88fad48ce71d18710a29da2fee6bf31
ngohungphuc <ngohungphuc95@gmail.com>
2021-04-26 20:36:44 +0700

    add

--- Swift-App/BMI-Calculator-iOS13/BMI Calculator/Views/ResultViewController.swift
+++ Swift-App/BMI-Calculator-iOS13/BMI Calculator/Views/ResultViewController.swift
@@ -9,13 +9,13 @@
 import UIKit
 
 class ResultViewController: UIViewController {
-
+    var bmiValue: String?
     @IBOutlet weak var bmiLabel: UILabel!
     @IBOutlet weak var adviceLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
     
@@ -23,13 +23,13 @@ class ResultViewController: UIViewController {
     }
     
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
