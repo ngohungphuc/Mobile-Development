21a4a1e13a77afaef441b85c99c8aa63d85d8c16
ngohungphuc <ngohungphuc95@gmail.com>
2021-04-30 17:00:12 +0700

    add

--- Swift-App/Clima-iOS13/Clima/Model/WeatherData.swift
+++ Swift-App/Clima-iOS13/Clima/Model/WeatherData.swift
@@ -8,18 +8,18 @@
 
 import Foundation
 
-struct WeatherData: Decodable {
+struct WeatherData: Codable {
     let name: String
     let main: Main
     let weather: [Weather]
 }
 
 
-struct Main: Decodable {
+struct Main: Codable {
     let temp: Double
 }
 
-struct Weather: Decodable {
+struct Weather: Codable {
     let id: Int
     let description: String
     
