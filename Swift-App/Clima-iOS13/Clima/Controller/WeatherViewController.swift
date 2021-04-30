ed4efd1d5c37e06af58cb2c4acb2570768863eec
ngohungphuc <ngohungphuc95@gmail.com>
2021-04-30 20:36:53 +0700

    add

--- Swift-App/Clima-iOS13/Clima/Controller/WeatherViewController.swift
+++ Swift-App/Clima-iOS13/Clima/Controller/WeatherViewController.swift
@@ -27,6 +27,9 @@ class WeatherViewController: UIViewController {
         weatherManager.delegate = self
         searchTextField.delegate = self
     }
+    @IBAction func locationPressed(_ sender: UIButton) {
+        locationManager.requestLocation()
+    }
 }
 
 //MARK: - UITextFieldDelegate
@@ -81,6 +84,7 @@ extension WeatherViewController: WeatherManagerDelegate {
 extension WeatherViewController: CLLocationManagerDelegate {
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         if let location = locations.last {
+            locationManager.stopUpdatingLocation()
             let lat = location.coordinate.latitude
             let lon = location.coordinate.longitude
             weatherManager.fetchWeather(lat, lon)
