dafa55f48417806affcda6ddd0ac63e799723d75
ngohungphuc <ngohungphuc95@gmail.com>
2021-05-02 21:29:29 +0700

    add

--- Swift-App/Flash-Chat-iOS13/Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Swift-App/Flash-Chat-iOS13/Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -14,9 +14,14 @@ class ChatViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var messageTextfield: UITextField!
     
+    var messages: [Message] = [
+        Message(sender: "1@2mail.com", body: "Hey")
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         title = K.appName
+        tableView.dataSource = self
         navigationItem.hidesBackButton = true
         
     }
@@ -31,7 +36,19 @@ class ChatViewController: UIViewController {
         } catch let signOutError as NSError {
             print ("Error signing out: %@", signOutError)
         }
-        
     }
+}
+
+extension ChatViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return messages.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: K.cellIdentifier, for: indexPath)
+        cell.textLabel?.text = messages[indexPath.row].body
+        return cell
+    }
+    
     
 }
