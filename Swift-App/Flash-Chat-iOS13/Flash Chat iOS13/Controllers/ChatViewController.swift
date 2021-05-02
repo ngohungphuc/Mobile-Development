2cd04208e273c7e88eddd09d8af0ea21a634a6ad
ngohungphuc <ngohungphuc95@gmail.com>
2021-05-02 22:09:26 +0700

    change  data

--- Swift-App/Flash-Chat-iOS13/Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Swift-App/Flash-Chat-iOS13/Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -45,8 +45,8 @@ extension ChatViewController: UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: K.cellIdentifier, for: indexPath)
-        cell.textLabel?.text = messages[indexPath.row].body
+        let cell = tableView.dequeueReusableCell(withIdentifier: K.cellIdentifier, for: indexPath) as! MessageCell
+        cell.label?.text = messages[indexPath.row].body
         return cell
     }
     
