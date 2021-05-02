17d8f6b7a150d7ad1a4bd732d5fdee10e4ce6f4e
ngohungphuc <ngohungphuc95@gmail.com>
2021-05-02 22:21:27 +0700

    update

--- Swift-App/Flash-Chat-iOS13/Flash Chat iOS13/Views/MessageCell.swift
+++ Swift-App/Flash-Chat-iOS13/Flash Chat iOS13/Views/MessageCell.swift
@@ -16,7 +16,7 @@ class MessageCell: UITableViewCell {
 
     override func awakeFromNib() {
         super.awakeFromNib()
-        messageBuble.layer.cornerRadius = messageBuble.frame.size.height
+        messageBuble.layer.cornerRadius = messageBuble.frame.size.height / 5 
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
