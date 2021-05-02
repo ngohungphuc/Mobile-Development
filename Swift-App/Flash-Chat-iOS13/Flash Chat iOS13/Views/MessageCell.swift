cd67cf0517b818d7c024d1f659a5bf20bb34ba66
ngohungphuc <ngohungphuc95@gmail.com>
2021-05-02 21:42:32 +0700

    add view

---
+++ Swift-App/Flash-Chat-iOS13/Flash Chat iOS13/Views/MessageCell.swift
@@ -0,0 +1,24 @@
+//
+//  MessageCell.swift
+//  Flash Chat iOS13
+//
+//  Created by Ngo Hung Phuc on 02/05/2021.
+//  Copyright © 2021 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class MessageCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
