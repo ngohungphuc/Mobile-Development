02d996a83fad49b87a6d79f94e7d6bb033802503
ngohungphuc <ngohungphuc95@gmail.com>
2021-05-03 19:36:09 +0700

    add swift UI project

---
+++ Swift-App/I-Am-Rich-SwiftUI/I-Am-Rich-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  I-Am-Rich-SwiftUI
+//
+//  Created by Ngo Hung Phuc on 03/05/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Swift-App/I-Am-Rich-SwiftUI/I-Am-Rich-SwiftUI/I_Am_Rich_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  I_Am_Rich_SwiftUIApp.swift
+//  I-Am-Rich-SwiftUI
+//
+//  Created by Ngo Hung Phuc on 03/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct I_Am_Rich_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
