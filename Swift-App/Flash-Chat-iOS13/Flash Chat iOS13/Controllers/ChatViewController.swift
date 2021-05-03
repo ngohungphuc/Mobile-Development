9f6c6ef0d11a8d7a1560f657190df82f7f30b301
ngohungphuc <ngohungphuc95@gmail.com>
2021-05-03 15:29:49 +0700

    add scrollToRow

--- Swift-App/Flash-Chat-iOS13/Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Swift-App/Flash-Chat-iOS13/Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -47,6 +47,8 @@ class ChatViewController: UIViewController {
                                 
                                 DispatchQueue.main.async {
                                     self.tableView.reloadData()
+                                    let indexPath = IndexPath(row: self.messages.count - 1, section: 0)
+                                    self.tableView.scrollToRow(at: indexPath, at: .top, animated: true)
                                 }
                             }
                         }
