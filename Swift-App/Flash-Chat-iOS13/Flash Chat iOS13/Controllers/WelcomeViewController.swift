15df10944ea3c1559ae4e245a056d15349fcbdd4
ngohungphuc <ngohungphuc95@gmail.com>
2021-05-03 15:38:45 +0700

    add isNavigationBarHidden

--- Swift-App/Flash-Chat-iOS13/Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Swift-App/Flash-Chat-iOS13/Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -13,6 +13,14 @@ class WelcomeViewController: UIViewController {
     
     @IBOutlet weak var titleLabel: CLTypingLabel!
     
+    override func viewWillAppear(_ animated: Bool) {
+        navigationController?.isNavigationBarHidden = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        navigationController?.isNavigationBarHidden = false
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
