67ed4b4e70ffd52dbaae6dbd335389a86e5726f4
ngohungphuc <ngohungphuc95@gmail.com>
2021-05-05 20:32:53 +0700

    add

--- Swift-App/TonyCard/TonyCard/ContentView.swift
+++ Swift-App/TonyCard/TonyCard/ContentView.swift
@@ -11,8 +11,10 @@ struct ContentView: View {
     var body: some View {
         ZStack {
             Color(red: 0.70, green: 0.75, blue: 0.76, opacity: 1.00).edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
-            Text("Hello, world!")
-                .padding()
+            Text("Tony Ngo")
+                .font(.title)
+                .bold()
+                .foregroundColor(.white)
         }
     }
 }
