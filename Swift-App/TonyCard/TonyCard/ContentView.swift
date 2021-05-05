7262e3a42505fabf14db0cf68d7367e530c961d9
ngohungphuc <ngohungphuc95@gmail.com>
2021-05-05 21:03:44 +0700

    add sub title

--- Swift-App/TonyCard/TonyCard/ContentView.swift
+++ Swift-App/TonyCard/TonyCard/ContentView.swift
@@ -11,10 +11,15 @@ struct ContentView: View {
     var body: some View {
         ZStack {
             Color(red: 0.70, green: 0.75, blue: 0.76, opacity: 1.00).edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
-            Text("Tony Ngô")
-                .font(Font.custom("Itim-Regular", size: 40))
-                .bold()
-                .foregroundColor(.white)
+            VStack {
+                Text("Tony Ngô")
+                    .font(Font.custom("Itim-Regular", size: 40))
+                    .bold()
+                    .foregroundColor(.white)
+                Text("Frontend Lead")
+                    .foregroundColor(.white)
+                    .font(size: 25)
+            }
         }
     }
 }
