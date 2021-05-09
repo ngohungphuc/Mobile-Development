7c1fbd75c9e8ca068ac2755388aee9e0a5dff356
ngohungphuc <ngohungphuc95@gmail.com>
2021-05-09 19:16:07 +0700

    add onAppear

--- Swift-App/News/News/ContentView.swift
+++ Swift-App/News/News/ContentView.swift
@@ -17,6 +17,9 @@ struct ContentView: View {
             }
             .navigationBarTitle("Hacker News")
         }
+        .onAppear {
+            networkManager.fetchData()
+        }
     }
 }
 
