9900e602d6b4a2b00f4f68231959cf76d0bd4928
ngohungphuc <ngohungphuc95@gmail.com>
2021-05-13 08:52:54 +0700

    add data

--- Swift-App/Todoey-iOS13/Todoey/TodoListViewController.swift
+++ Swift-App/Todoey-iOS13/Todoey/TodoListViewController.swift
@@ -17,6 +17,14 @@ class TodoListViewController: UITableViewController {
     }
     
     //MARK - Tableview Datasource Methods
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return item.count
+    }
     
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        cell.textLabel?.text = item[indexPath.row]
+        return cell
+    }
 }
 
