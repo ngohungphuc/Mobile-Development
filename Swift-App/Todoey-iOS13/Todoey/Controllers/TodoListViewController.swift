91a4faeb5287989b1c2311cab876870b035621ef
ngohungphuc <ngohungphuc95@gmail.com>
2021-05-21 09:41:37 +0700

    add

--- Swift-App/Todoey-iOS13/Todoey/Controllers/TodoListViewController.swift
+++ Swift-App/Todoey-iOS13/Todoey/Controllers/TodoListViewController.swift
@@ -11,8 +11,6 @@ import CoreData
 
 class TodoListViewController: UITableViewController {
     var itemArray = [Item]()
-    
-    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
     let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     
     override func viewDidLoad() {
