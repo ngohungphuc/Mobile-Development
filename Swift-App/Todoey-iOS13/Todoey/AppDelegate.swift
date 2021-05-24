fcfb56ebbcbf6aab4ffce6c6cc8382b52c3def72
ngohungphuc <ngohungphuc95@gmail.com>
2021-05-24 23:00:14 +0700

    add

--- Swift-App/Todoey-iOS13/Todoey/AppDelegate.swift
+++ Swift-App/Todoey-iOS13/Todoey/AppDelegate.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import CoreData
+import RealmSwift
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -17,6 +18,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        do {
+            let realm = try Realm()
+        } catch {
+            print(error)
+        }
+        
         return true
     }
     
---
+++ Swift-App/Todoey-iOS13/Todoey/DataModel/Data.swift
@@ -0,0 +1,15 @@
+//
+//  Data.swift
+//  Todoey
+//
+//  Created by Ngo Hung Phuc on 24/05/2021.
+//  Copyright © 2021 App Brewery. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class Data: Object {
+    var name: String = ""
+    var age: Int = 0
+}
