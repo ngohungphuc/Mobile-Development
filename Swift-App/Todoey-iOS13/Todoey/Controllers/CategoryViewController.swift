38789edaddd66b057733d3e58249fb3a092d415f
ngohungphuc <ngohungphuc95@gmail.com>
2021-05-24 21:50:50 +0700

    add

--- Swift-App/Todoey-iOS13/Todoey/Controllers/CategoryViewController.swift
+++ Swift-App/Todoey-iOS13/Todoey/Controllers/CategoryViewController.swift
@@ -35,15 +35,13 @@ class CategoryViewController: UITableViewController {
         return categories.count
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell", for: indexPath)
 
         // Configure the cell...
 
         return cell
     }
-    */
 
     /*
     // Override to support conditional editing of the table view.
