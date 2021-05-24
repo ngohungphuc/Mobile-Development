7e21752332b5953879b77e10ff7bc122946b36df
ngohungphuc <ngohungphuc95@gmail.com>
2021-05-24 22:09:52 +0700

    add

--- Swift-App/Todoey-iOS13/Todoey/Controllers/CategoryViewController.swift
+++ Swift-App/Todoey-iOS13/Todoey/Controllers/CategoryViewController.swift
@@ -17,12 +17,6 @@ class CategoryViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-        
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
-        
         loadCategories()
     }
     
@@ -45,68 +39,26 @@ class CategoryViewController: UITableViewController {
         return cell
     }
     
-    /*
-     // Override to support conditional editing of the table view.
-     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-     // Return false if you do not want the specified item to be editable.
-     return true
-     }
-     */
-    
-    /*
-     // Override to support editing the table view.
-     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-     if editingStyle == .delete {
-     // Delete the row from the data source
-     tableView.deleteRows(at: [indexPath], with: .fade)
-     } else if editingStyle == .insert {
-     // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-     }
-     }
-     */
-    
-    /*
-     // Override to support rearranging the table view.
-     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-     
-     }
-     */
-    
-    /*
-     // Override to support conditional rearranging of the table view.
-     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-     // Return false if you do not want the item to be re-orderable.
-     return true
-     }
-     */
-    
-    /*
-     // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destination.
-     // Pass the selected object to the new view controller.
-     }
-     */
     func saveCategories() {
         do {
             try context.save()
+        } catch {
+            print("Error saving category \(error)")
         }
-        catch {
-            print(error)
-        }
+        
         tableView.reloadData()
     }
     
     func loadCategories() {
-        let request: NSFetchRequest<Category> = Category.fetchRequest()
-        do {
+        
+        let request : NSFetchRequest<Category> = Category.fetchRequest()
+        
+        do{
             categories = try context.fetch(request)
+        } catch {
+            print("Error loading categories \(error)")
         }
-        catch {
-            print(error)
-        }
+        
         tableView.reloadData()
     }
     
@@ -114,9 +66,10 @@ class CategoryViewController: UITableViewController {
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
         var textField = UITextField()
         let alert = UIAlertController(title: "Add New Cateogry", message: "", preferredStyle: .alert)
+        
         let action = UIAlertAction(title: "Add", style: .default) { action in
             let newCategory = Category(context: self.context)
-            newCategory.name = textField.text
+            newCategory.name = textField.text!
             
             self.categories.append(newCategory)
             
