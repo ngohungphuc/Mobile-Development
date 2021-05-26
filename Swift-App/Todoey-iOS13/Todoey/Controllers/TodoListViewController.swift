766114aff0c5a51e81689799b19f631c9e36c753
ngohungphuc <ngohungphuc95@gmail.com>
2021-05-26 20:18:12 +0700

    add

--- Swift-App/Todoey-iOS13/Todoey/Controllers/TodoListViewController.swift
+++ Swift-App/Todoey-iOS13/Todoey/Controllers/TodoListViewController.swift
@@ -75,6 +75,7 @@ class TodoListViewController: UITableViewController {
                     try self.realm.write {
                         let newItem = Item()
                         newItem.title = textField.text!
+                        newItem.dateCreated = Date()
                         currentCategory.items.append(newItem)
                     }
                 } catch {
@@ -107,7 +108,9 @@ class TodoListViewController: UITableViewController {
 
 extension TodoListViewController: UISearchBarDelegate {
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
-        todoItems = todoItems?.filter("title CONTAINS[cd] %@", searchBar.text!).sorted(byKeyPath: "title", ascending: true)
+        todoItems = todoItems?.filter("title CONTAINS[cd] %@", searchBar.text!).sorted(byKeyPath: "dateCreated", ascending: true)
+        
+        tableView.reloadData()
     }
 
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
--- Swift-App/Todoey-iOS13/Todoey/DataModel/Item.swift
+++ Swift-App/Todoey-iOS13/Todoey/DataModel/Item.swift
@@ -12,5 +12,6 @@ import RealmSwift
 class Item : Object {
     @objc dynamic var title:String = ""
     @objc dynamic var done:Bool = false
+    @objc dynamic var dateCreated:Date?
     var parentCategory = LinkingObjects(fromType: Category.self, property: "items")
 }
