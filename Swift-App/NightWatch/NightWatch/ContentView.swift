0d3f672ff89917b06059d18f547c2883b8e77598
ngohungphuc <ngohungphuc95@gmail.com>
2021-06-07 11:12:50 +0700

    add

---
+++ Swift-App/NightWatch/NightWatch/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NightWatch
+//
+//  Created by Ngo Hung Phuc on 07/06/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Swift-App/NightWatch/NightWatch/NightWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  NightWatchApp.swift
+//  NightWatch
+//
+//  Created by Ngo Hung Phuc on 07/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct NightWatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
