9c5fcb22ad8b1f48cae7603658b0415c5d9bf84e
ngohungphuc <ngohungphuc95@gmail.com>
2021-06-28 19:31:43 +0700

    add

--- Swift/WiredBrainCoffee-XcodeProject/WiredBrainCoffee/Scenes/GiftViewController/GiftViewController.swift
+++ Swift/WiredBrainCoffee-XcodeProject/WiredBrainCoffee/Scenes/GiftViewController/GiftViewController.swift
@@ -10,9 +10,26 @@ import UIKit
 
 class GiftViewController: UIViewController {
 
+    @IBOutlet weak var seasonCollectionView: UICollectionView!
+    let colorData = [#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0), #colorLiteral(red: 0.1019607857, green: 0.2784313858, blue: 0.400000006, alpha: 1)]
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        seasonCollectionView.dataSource  = self
     }
+}
+
+extension GiftViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return colorData.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "GiftCardCell", for: indexPath)
+        
+    }
+    
+    
 }
