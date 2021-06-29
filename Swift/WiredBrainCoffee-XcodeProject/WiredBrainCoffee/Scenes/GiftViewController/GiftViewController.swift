ca4ea2cb4474713053de74512956d82844d2c339
ngohungphuc <ngohungphuc95@gmail.com>
2021-06-29 21:28:36 +0700

    add

--- Swift/WiredBrainCoffee-XcodeProject/WiredBrainCoffee/Scenes/GiftViewController/GiftViewController.swift
+++ Swift/WiredBrainCoffee-XcodeProject/WiredBrainCoffee/Scenes/GiftViewController/GiftViewController.swift
@@ -46,9 +46,17 @@ extension GiftViewController: UICollectionViewDataSource, UICollectionViewDelega
     }
     
     func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        if kind == UICollectionView.elementKindSectionHeader {
+            let view = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "sectionHeader", for: indexPath) as! HeaderCollectionReusableView
+            view.setup(count: colorData.count)
+            return view
+        }
+        else {
+            let view = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "sectionFooter", for: indexPath) as! HeaderCollectionReusableView
+            view.setup(count: colorData.count)
+            view.backgroundColor = UIColor.purple
+            return view
+        }
         
-        let view = collectionView.dequeueReusableSupplementaryView(ofKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: "sectionHeader", for: indexPath) as! HeaderCollectionReusableView
-        view.setup(count: colorData.count)
-        return view
     }
 }
