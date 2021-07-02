e813fab28b9dd2164cd5eedd08a9a60e9b3082f1
ngohungphuc <ngohungphuc95@gmail.com>
2021-07-02 11:02:31 +0700

    add

--- Swift/WiredBrainCoffee-XcodeProject/WiredBrainCoffee/Scenes/GiftViewController/ColorCollectionViewCell.swift
+++ Swift/WiredBrainCoffee-XcodeProject/WiredBrainCoffee/Scenes/GiftViewController/ColorCollectionViewCell.swift
@@ -12,6 +12,15 @@ class ColorCollectionViewCell: UICollectionViewCell {
     
     @IBOutlet weak var numberLabel: UILabel!
     
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        layer.cornerRadius = 10
+        layer.shadowOpacity = 1
+        layer.shadowOffset = CGSize(width: 1, height: 1)
+    }
+    
+    
     func setup(backgroundColor: UIColor, cellNumber: Int) {
         self.backgroundColor = backgroundColor;
         numberLabel.text = "\(cellNumber)"
