cca666ad53e0c98685c639e867b5048e02489f26
ngohungphuc <ngohungphuc95@gmail.com>
2021-07-02 11:33:31 +0700

    add

---
+++ Swift/WiredBrainCoffee-XcodeProject/WiredBrainCoffee/Scenes/GiftViewController/GiftCardCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  GiftCardCollectionViewCell.swift
+//  WiredBrainCoffee
+//
+//  Created by Ngo Hung Phuc on 02/07/2021.
+//  Copyright © 2021 Mark Moeykens. All rights reserved.
+//
+
+import UIKit
+
+class GiftCardCollectionViewCell: UICollectionViewCell {
+    
+}
