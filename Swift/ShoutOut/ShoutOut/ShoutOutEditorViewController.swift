fd6e6d069eb695aabce5343afb82ba63ec0e827d
ngohungphuc <ngohungphuc95@gmail.com>
2021-07-28 15:32:50 +0700

    add

--- Swift/ShoutOut/ShoutOut/ShoutOutEditorViewController.swift
+++ Swift/ShoutOut/ShoutOut/ShoutOutEditorViewController.swift
@@ -23,16 +23,38 @@ class ShoutOutEditorViewController: UIViewController,
         "Amazing Effort!"
     ]
     
+    var employees: [Employee]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        fetchEmployees()
+        
+        self.toEmployeePicker.dataSource = self
+        self.toEmployeePicker.delegate = self
+        self.toEmployeePicker.tag = 0
         self.shoutCategoryPicker.dataSource = self
         self.shoutCategoryPicker.delegate = self
+        self.shoutCategoryPicker.tag = 1
         
         messageTextView.layer.borderWidth = CGFloat(0.5)
         messageTextView.layer.borderColor = UIColor(red: 204/255, green: 204/255, blue: 204/255, alpha: 1.0).cgColor
         messageTextView.layer.cornerRadius = 5
         messageTextView.clipsToBounds = true
+    }
+    
+    func fetchEmployees() {
+        let fetchRequest = NSFetchRequest<Employee>(entityName: Employee.entityName)
+        let primarySortDescriptor = NSSortDescriptor(key: #keyPath(Employee.lastName), ascending: true)
+        let secondarySortDescriptor = NSSortDescriptor(key: #keyPath(Employee.firstName), ascending: true)
+        
+        fetchRequest.sortDescriptors = [primarySortDescriptor, secondarySortDescriptor]
+        
+        do {
+            self.employees = try self.managedObjectContext.fetch(fetchRequest)
+        } catch {
+            self.employees = []
+        }
     }
     
     @IBAction func cancelButtonTapped(_ sender: UIBarButtonItem) {
@@ -50,13 +72,19 @@ class ShoutOutEditorViewController: UIViewController,
     func pickerView(_ pickerView: UIPickerView,
                     numberOfRowsInComponent component: Int) -> Int {
         
-        return self.shoutCategories.count
+        return pickerView.tag == 0 ? self.employees.count : self.shoutCategories.count
     }
     
     func pickerView(_ pickerView: UIPickerView,
                     titleForRow row: Int,
                     forComponent component: Int) -> String? {
         
-        return shoutCategories[row]
+        if pickerView.tag == 0 {
+                    let employee = self.employees[row]
+                    
+                    return "\(employee.firstName) \(employee.lastName)"
+                } else {
+                    return shoutCategories[row]
+                }
     }
 }
