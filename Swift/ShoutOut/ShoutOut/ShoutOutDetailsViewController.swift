22dd185fe3ecc7258994863ade9337831a0a3543
ngohungphuc <ngohungphuc95@gmail.com>
2021-07-28 15:59:07 +0700

    add

--- Swift/ShoutOut/ShoutOut/ShoutOutDetailsViewController.swift
+++ Swift/ShoutOut/ShoutOut/ShoutOutDetailsViewController.swift
@@ -23,4 +23,32 @@ class ShoutOutDetailsViewController: UIViewController,
         let destinationVC = navigationController.viewControllers[0] as! ShoutOutEditorViewController
         destinationVC.managedObjectContext = self.managedObjectContext
     }
+    
+    @IBAction func deleteButtonTapped(_ sender: UIBarButtonItem) {
+        let alertController = UIAlertController(title: "Delete ShoutOut",
+                                                message: "Are you sure you want to delete this ShoutOut?",
+                                                preferredStyle: .actionSheet)
+        
+        let deleteAction = UIAlertAction(title: "Delete", style: .destructive) {
+            (_) -> Void in
+            
+            self.managedObjectContext.delete(self.shoutOut)
+            
+            do {
+                try self.managedObjectContext.save()
+            } catch {
+                self.managedObjectContext.rollback()
+                print("Something went wrong: \(error)")
+            }
+            
+            let _ = self.navigationController?.popViewController(animated: true)
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        
+        alertController.addAction(deleteAction)
+        alertController.addAction(cancelAction)
+        
+        self.present(alertController, animated: true, completion: nil)
+    }
 }
