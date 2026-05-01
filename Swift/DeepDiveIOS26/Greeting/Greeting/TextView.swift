c8e8becdb163abccf508b0a07b0ac5a60215bf08
ngohungphuc <ngohungphuc95@gmail.com>
2026-05-01 11:43:40 +0200

    update

--- Swift/DeepDiveIOS26/Greeting/Greeting/TextView.swift
+++ Swift/DeepDiveIOS26/Greeting/Greeting/TextView.swift
@@ -9,7 +9,20 @@ import SwiftUI
 
 struct TextView: View {
     let text: String
-    let color: Color
+    @State  var color: Color
+    
+    let colors: [Color] = [
+        .red,
+        .green,
+        .blue,
+        .orange,
+        .yellow,
+        .purple,
+        Color(red: 0.5, green: 0, blue: 0.5),
+        Color(red: 0, green: 0.5, blue: 0.5),
+        Color(red: 139/255, green: 207/255, blue: 240/255),
+        Color(red: 1, green: 215/255, blue: 0),
+    ]
     
     var body: some View {
         Text(text)
@@ -19,6 +32,9 @@ struct TextView: View {
             .background(color.opacity(0.4))
             .cornerRadius(20.0)
             .shadow(color:color, radius: 5, x:10, y:10)
+            .onTapGesture {
+                color = colors.randomElement()!
+            }
     }
 }
 
